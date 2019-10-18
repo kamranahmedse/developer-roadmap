fb197ae3b3bca6220c024b2c63fa19250369f96a
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-10-18 23:40:37 +0400

    Add build and static exports

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS(withSass({
+  exportPathMap: () => {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/privacy': { page: '/privacy' },
+      '/terms': { page: '/terms' },
+    };
+  },
+
   webpack(config, options) {
     config.module.rules.push({
       test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
