1ae125b184b6b5ab1809555bc9540c36c143e7d0
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-11-10 21:10:13 +0400

    Fix - Error on the homepage

--- components/featured-content/index.js
+++ components/featured-content/index.js
@@ -1,7 +1,6 @@
 import { FeaturedWrap } from './style';
 import FeaturedGuides from './guides';
 import FeaturedRoadmaps from './roadmaps';
-import FeaturedJourneys from './journeys';
 
 const FeaturedContent = (props) => (
   <FeaturedWrap className="border-top bg-light">
