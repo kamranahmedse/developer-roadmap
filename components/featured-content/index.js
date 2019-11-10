dcef934aefb09415c930d7a19852db217a203e28
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-11-10 20:49:34 +0400

    Remove upcoming page

--- components/featured-content/index.js
+++ components/featured-content/index.js
@@ -1,6 +1,7 @@
 import { FeaturedWrap } from './style';
 import FeaturedGuides from './guides';
 import FeaturedRoadmaps from './roadmaps';
+import FeaturedJourneys from './journeys';
 
 const FeaturedContent = (props) => (
   <FeaturedWrap className="border-top bg-light">
