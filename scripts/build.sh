f8fcb8d6003e3b6d03e122e97bf292a6c1c9b72c
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-11-16 16:12:18 +0400

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-ENV_FILE=.env
-
-## Make sure that the configuration file exists before continuing
-if [ ! -f "$ENV_FILE" ]; then
-  echo "$ENV_FILE does not exist"
-  exit 1
-fi
-
 NODE_ENV=prod next build
 next export
 echo 'roadmap.sh' > out/CNAME
