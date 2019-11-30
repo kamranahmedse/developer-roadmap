2468a9785655a618ad08bcb52f6553b2914ae912
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-11-30 14:27:37 +0400

    Split roadmap summaries and detailed roadmap

--- components/roadmap-body/style.js
+++ components/roadmap-body/style.js
@@ -13,10 +13,69 @@ export const UpcomingContainer = styled.div`
 
 export const Summary = styled.div`
   text-align: center;
-  padding: 40px 0 50px;
   min-height: 400px;
-  
+  display: flex;
+
   .container {
     position: relative;
+  }
+`;
+
+export const Sidebar = styled.div`
+  border-left: 1px solid #efefef;
+  padding-bottom: 150px;
+
+  ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+  }
+  
+  .bullet {
+    display: inline-block;
+    margin-right: 7px;
+    width: 7px;
+    height: 7px;
+    min-width: 7px;
+    border-radius: 100%;
+    background: #efefef;
+    transform: translateX(-4px);
+    transition: background 0.5s ease;
+  }
+
+  
+  .links-group {
+    padding: 30px 0 10px;
+    width: 260px;
+
+    h3 {
+      font-size: 12px;
+      font-weight: 600;
+      text-transform: uppercase;
+      padding-left: 20px;
+    }
+  }
+  
+  .links-group li {
+    list-style: none;
+    margin: 7px 0;
+
+    a {
+      font-size: 14px;
+      font-weight: normal;
+      color: #696969;
+    }
+    
+    .bullet {
+      display: inline-block;
+      margin-right: 12px;
+      width: 7px;
+      height: 7px;
+      min-width: 7px;
+      border-radius: 100%;
+      background: #efefef;
+      transform: translateX(-4px);
+      transition: background 0.5s ease;
+    }
   }
 `;
---
+++ components/roadmap-summary/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const SummaryContainer = styled.div``;
+
+export const UpcomingContainer = styled.div`
+  text-align: center;
+  padding: 40px 0 50px;
+
+  .container {
+    position: relative;
+  }
+`;
+
+export const Summary = styled.div`
+  margin-top: 35px;
+  min-height: 400px;
+  display: block;
+  position: relative;
+  text-align: left;
+`;
