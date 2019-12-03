d374f642722a419a4e4f1021e7946f48bc1ac664
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-12-03 17:54:34 +0400

    Fix - Duplicated meta tags on pages

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import FeaturedContent from 'components/featured-content/index';
 import HeroSection from 'components/hero-section/index';
 import PageFooter from 'components/page-footer/index';
 import SiteNav from 'components/site-nav';
-import DefaultLayout from 'layouts/default/index';
+import Helmet from 'components/helmet';
+import DefaultLayout from 'layouts/default';
 
 const Home = (props) => (
   <DefaultLayout>
+    <Helmet />
     <SiteNav />
     <HeroSection />
     <FeaturedContent />
