77a66fd25d8e2d75a55fe5f708f176587fe78d62
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-01-21 00:20:25 +0400

    Add sponsors page

---
+++ components/mark/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Mark = styled.span`
+  background: #1b1e21;
+  color: #fff;
+  display: inline-block;
+  font-size: 18px;
+  font-weight: 600;
+  margin: 0 4px;
+  padding: 0 6px 0 7px;
+`;
