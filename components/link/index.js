f976013392cf6d43832a62c136c1e896fe10c724
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-01-21 00:20:25 +0400

    Add sponsors page

---
+++ components/link/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StrongLink  = styled.a`
+  border-bottom: 2px solid currentColor;
+  position: relative;
+  transition: background-color 120ms;
+  text-decoration: none;
+  font-weight: 600;
+  
+  &:hover {
+    text-decoration: none;
+  }
+`;
