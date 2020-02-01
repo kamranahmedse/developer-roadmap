d26008d990b29585cdf1673019099475931539bf
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-02-01 17:08:34 +0400

    Add sponsor information

--- components/sponsor-banner/style.js
+++ components/sponsor-banner/style.js
@@ -17,10 +17,18 @@ export const BannerWrap = styled.a`
   }
 `;
 
-export const EmojiWrap = styled.span`
+export const SponsorLogo = styled.img`
+  height: 20px;
+  margin-right: 10px;
   position: relative;
-  top: 1px;
-  margin: 0 0 0 6px;
+  top: -2px;
+`;
+
+export const EmojiWrap = styled.img`
+  height: 18px;
+  position: relative;
+  top: -1px;
+  margin: 0 6px 0 6px;
 `;
 
 export const CloseSponsor = styled.span`
