6976202171ed6e73c1c26a260286c9149c554efe
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-05-25 22:34:16 +0400

    Replace homepage with roadmaps page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import FeaturedContent from 'components/featured-content/index';
-import HeroSection from 'components/hero-section/index';
-import PageFooter from 'components/page-footer/index';
-import SiteNav from 'components/site-nav';
-import Helmet from 'components/helmet';
-import DefaultLayout from 'layouts/default/index';
+import RoadmapsList from './roadmaps';
 
 const Home = (props) => (
-  <DefaultLayout>
-    <Helmet />
-    <SiteNav />
-    <HeroSection />
-    <FeaturedContent />
-    <PageFooter />
-  </DefaultLayout>
+  <RoadmapsList />
 );
 
 export default Home;
