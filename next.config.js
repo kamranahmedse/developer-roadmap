c37297f8223a7ef0b903de49399f98d8562350e7
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-08-20 15:55:39 +0200

    Add support for markdown

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
-module.exports = {
+const path = require('path');
+const rehypePrism = require('@mapbox/rehype-prism');
+
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)?$/,
+  options: {
+    rehypePlugins: [rehypePrism]
+  }
+});
+
+let nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-}
+
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve('./'));
+
+    return config;
+  }
+};
+
+nextConfig = withMDX(nextConfig);
+
+module.exports = nextConfig;
