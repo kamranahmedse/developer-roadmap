67d2f5cb57f53ef3f40acd00e79da1a6a1406fde
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-09-05 20:38:08 +0200

    Add support for environment variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,30 @@
 const path = require('path');
+const fs = require('fs');
 const rehypePrism = require('@mapbox/rehype-prism');
+
+/**
+ * Loads the configuration for the given environment
+ * @param env
+ * @returns {*}
+ */
+const loadConfig = (env = 'dev') => {
+  const configPath = `./config/${env}.json`;
+  if (!fs.existsSync(configPath)) {
+    console.warn(`Config file not found: ${configPath}. Using environment variables only.`);
+  }
+
+  const appConfig = {};
+
+  for (let key in process.env) {
+    if (!key.startsWith('ROADMAP_')) {
+      continue;
+    }
+
+    appConfig[key.replace('ROADMAP_', '')] = process.env[key];
+  }
+
+  return appConfig;
+};
 
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)?$/,
@@ -11,6 +36,7 @@ const withMDX = require('@next/mdx')({
 let nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  env: loadConfig(process.env.NODE_ENV),
 
   webpack(config, options) {
     config.resolve.modules.push(path.resolve('./'));
