3ee4d49d0598deb8bcf9504c5a53e5f6537e9028
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-09-05 20:00:07 +0200

    Fix linting issues

--- components/custom-ad.tsx
+++ components/custom-ad.tsx
@@ -1,37 +1,33 @@
 export const CustomAd = () => {
   return (
-    <div id="carbonads">
+    <div id='carbonads'>
       <span>
-        <span className="carbon-wrap">
+        <span className='carbon-wrap'>
           <a
-            href="https://freemote.com/strategy?sl=roadmap"
-            className="carbon-img"
-            target="_blank"
-            rel="noopener sponsored"
+            href='https://freemote.com/strategy?sl=roadmap'
+            className='carbon-img'
+            target='_blank'
           >
             <img
-              src="/fm-img.png"
-              alt="FM Logo"
-              border="0"
-              height="100"
-              width="130"
-              style={{ maxWidth: "130px" }}
+              src='/fm-img.png'
+              alt='FM Logo'
+              height='100'
+              width='130'
+              style={{ maxWidth: '130px', border: 'none' }}
             />
           </a>
           <a
-            href="https://freemote.com/strategy?sl=roadmap"
-            className="carbon-text"
-            target="_blank"
-            rel="noopener sponsored"
+            href='https://freemote.com/strategy?sl=roadmap'
+            className='carbon-text'
+            target='_blank'
           >
             He Went from ZERO TO $74,000 as a Full Time Developer in 7 Weeks
           </a>
         </span>
         <a
-          href="https://github.com/sponsors/kamranahmedse"
-          className="carbon-poweredby"
-          target="_blank"
-          rel="noopener sponsored"
+          href='https://github.com/sponsors/kamranahmedse'
+          className='carbon-poweredby'
+          target='_blank'
         >
           Sponsored by
         </a>
