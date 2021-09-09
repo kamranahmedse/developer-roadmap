68950ab2c985819064173f69f5818d724e7af0fa
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-09-09 22:57:34 +0200

    Add helmet to thanks page

--- pages/thanks.tsx
+++ pages/thanks.tsx
@@ -17,11 +17,13 @@ import { OpensourceBanner } from '../components/opensource-banner';
 import { UpdatesBanner } from '../components/updates-banner';
 import { Footer } from '../components/footer';
 import siteConfig from '../content/site.json';
+import Helmet from '../components/helmet';
 
 export default function Thanks() {
   return (
     <Box bg='gray.50' minH='100vh'>
       <GlobalHeader />
+      <Helmet title={'Thanks to Sponsors'} />
       <Box mb='60px'>
         <Container maxW={'container.sm'} position='relative'>
           <Box mt='60px'>
@@ -74,18 +76,24 @@ export default function Thanks() {
             <Box mt='40px'>
               <Heading mb='15px' fontSize='25px' as='h2'>Monthly Sponsors</Heading>
               <UnorderedList>
-                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500} href='https://github.com/Stuart-cmd'>Stuart Bowles</Link></ListItem>
-                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500} href='https://github.com/MelnikovAG'>Alexander
+                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500}
+                                         href='https://github.com/Stuart-cmd'>Stuart Bowles</Link></ListItem>
+                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500}
+                                         href='https://github.com/MelnikovAG'>Alexander
                   Blake</Link></ListItem>
-                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500} href='https://github.com/josephjacks'>Joseph
+                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500}
+                                         href='https://github.com/josephjacks'>Joseph
                   Jacks</Link></ListItem>
-                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500} href='https://github.com/MustafaMagdi'>Mustafa
+                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500}
+                                         href='https://github.com/MustafaMagdi'>Mustafa
                   Magdi</Link></ListItem>
-                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500} href='https://github.com/dmytbolko'>Dmytro
+                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500}
+                                         href='https://github.com/dmytbolko'>Dmytro
                   Bolkachov</Link></ListItem>
                 <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500}
                                          href='https://github.com/liquidaty'>Liquidaty</Link></ListItem>
-                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500} href='https://github.com/Lazy-AL'>Anri
+                <ListItem mb='5px'><Link color='blue.500' target='_blank' fontWeight={500}
+                                         href='https://github.com/Lazy-AL'>Anri
                   Lazash</Link></ListItem>
               </UnorderedList>
             </Box>
