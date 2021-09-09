adc861e61d2c44e8b64f49005840ee1468ca3d07
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-09-09 21:48:31 +0200

    Update sponsor ads and carbon ad styling

--- components/custom-ad.tsx
+++ components/custom-ad.tsx
@@ -1,24 +1,23 @@
 export const CustomAd = () => {
   return (
-    <div id='carbonads'>
+    <div id='sponsorads'>
       <span>
-        <span className='carbon-wrap'>
+        <span className='sponsor-wrap'>
           <a
             href='https://freemote.com/strategy?sl=roadmap'
-            className='carbon-img'
+            className='sponsor-img'
             target='_blank'
           >
             <img
               src='/fm-img.png'
               alt='FM Logo'
-              height='100'
-              width='130'
-              style={{ maxWidth: '130px', border: 'none' }}
+              width='112'
+              style={{ maxWidth: '112px', border: 'none' }}
             />
           </a>
           <a
             href='https://freemote.com/strategy?sl=roadmap'
-            className='carbon-text'
+            className='sponsor-text'
             target='_blank'
           >
             He Went from ZERO TO $74,000 as a Full Time Developer in 7 Weeks
@@ -26,7 +25,7 @@ export const CustomAd = () => {
         </span>
         <a
           href='https://github.com/sponsors/kamranahmedse'
-          className='carbon-poweredby'
+          className='sponsor-poweredby'
           target='_blank'
         >
           Sponsored by
