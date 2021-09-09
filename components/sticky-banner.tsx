b6205af02c5fdc584401c1171b46994c1e9e2533
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-09-09 22:09:20 +0200

    Change youtube banner color

--- components/sticky-banner.tsx
+++ components/sticky-banner.tsx
@@ -6,18 +6,18 @@ export function StickyBanner() {
   return (
     <Flex as={Link}
           href={siteConfig.url.youtube}
-          bg={'teal.900'}
-          color='gray.300'
+          bg={'yellow.300'}
+          color='gray.900'
           alignItems='center'
           justifyContent='center'
           py='7px'
-          _hover={{ textDecoration: 'none', bg: 'teal.800', color: 'gray.100' }}
+          _hover={{ textDecoration: 'none', bg: 'yellow.400', }}
           target='_blank'
     >
       <YouTubeLogo style={{ height: '20px', display: 'inline-block', marginRight: '7px' }} />
       <Text as='span' fontWeight={500} fontSize='14px'>
-        <Text as='span' d={['none', 'block']}>Subscribe to our YouTube channel for the video content.</Text>
-        <Text as='span' d={['block', 'none']}>We now have a YouTube Channel.</Text>
+        <Text as='span'>We now have a YouTube Channel. <Text as='span' d={['none', 'inline']}>Subscribe for the video
+          content.</Text></Text>
       </Text>
     </Flex>
   );
