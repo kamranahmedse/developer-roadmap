82456021ad6be5ddf2eaad19fb0502332085d2d6
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-12-07 13:10:17 +0100

    Refactor roadmap header

--- components/roadmap/roadmap-page-header.tsx
+++ components/roadmap/roadmap-page-header.tsx
@@ -1,48 +1,92 @@
 import { RoadmapType } from '../../lib/roadmap';
 import { NewAlertBanner } from './new-alert-banner';
-import { Button, Link, Stack, Tab, TabList, Tabs, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import { AtSignIcon, DownloadIcon } from '@chakra-ui/icons';
-import { PageHeader } from '../page-header';
 import React from 'react';
 
 type RoadmapPageHeaderType = {
   roadmap: RoadmapType;
-}
+};
 
 export function RoadmapPageHeader(props: RoadmapPageHeaderType) {
   const { roadmap } = props;
 
   return (
-    <PageHeader
-      beforeTitle={<NewAlertBanner />}
-      title={roadmap.title}
-      subtitle={roadmap.description}
+    <Box
+      pt={['25px', '20px', '45px']}
+      pb={['20px', '15px', '30px']}
+      borderBottomWidth={1}
+      mb="30px"
     >
-      <Stack mt='20px' isInline>
-        <Button d={['flex', 'flex']} as={Link} href={'/roadmaps'} size='xs' py='14px' px='10px'
-                colorScheme='teal' variant='solid' _hover={{ textDecoration: 'none' }}>
-          &larr; <Text as='span' d={['none', 'inline']} ml='5px'>All Roadmaps</Text>
-        </Button>
-
-        {roadmap.pdfUrl && (
-          <Button as={Link}
-                  href={roadmap.pdfUrl}
-                  target='_blank'
-                  size='xs'
-                  py='14px'
-                  px='10px'
-                  leftIcon={<DownloadIcon />}
-                  colorScheme='yellow'
-                  variant='solid'
-                  _hover={{ textDecoration: 'none' }}>
-            Download
+      <Container maxW="container.md" position="relative">
+        <NewAlertBanner />
+        <Heading
+          as="h1"
+          color="black"
+          fontSize={['28px', '33px', '40px']}
+          fontWeight={700}
+          mb={['2px', '2px', '5px']}
+        >
+          {roadmap.title}
+        </Heading>
+        <Text fontSize={['13px', '14px', '15px']}>{roadmap.description}</Text>
+        <Stack mt="20px" isInline>
+          <Button
+            d={['flex', 'flex']}
+            as={Link}
+            href={'/roadmaps'}
+            size="xs"
+            py="14px"
+            px="10px"
+            colorScheme="teal"
+            variant="solid"
+            _hover={{ textDecoration: 'none' }}
+          >
+            &larr;
+            <Text as="span" d={['none', 'inline']} ml="5px">
+              All Roadmaps
+            </Text>
           </Button>
-        )}
-        <Button as={Link} href={'/signup'} size='xs' py='14px' px='10px' leftIcon={<AtSignIcon />}
-                colorScheme='yellow' variant='solid' _hover={{ textDecoration: 'none' }}>
-          Subscribe
-        </Button>
-      </Stack>
-    </PageHeader>
+
+          {roadmap.pdfUrl && (
+            <Button
+              as={Link}
+              href={roadmap.pdfUrl}
+              target="_blank"
+              size="xs"
+              py="14px"
+              px="10px"
+              leftIcon={<DownloadIcon />}
+              colorScheme="yellow"
+              variant="solid"
+              _hover={{ textDecoration: 'none' }}
+            >
+              Download
+            </Button>
+          )}
+          <Button
+            as={Link}
+            href={'/signup'}
+            size="xs"
+            py="14px"
+            px="10px"
+            leftIcon={<AtSignIcon />}
+            colorScheme="yellow"
+            variant="solid"
+            _hover={{ textDecoration: 'none' }}
+          >
+            Subscribe
+          </Button>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
