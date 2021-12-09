6e451ef5cf923962e168485f0d9c6a861fc56b6a
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-12-10 00:52:40 +0100

    Add JavaScript and VCS content and fix rendering issue

--- lib/renderer/utils.ts
+++ lib/renderer/utils.ts
@@ -1,3 +1,21 @@
+export function queryGroupElementsById(groupId: string) {
+  const elements = document.querySelectorAll(
+    `[data-group-id$="-${groupId}"]`
+  ) as any;
+  const matchingElements: HTMLElement[] = [];
+
+  elements.forEach((element: HTMLElement) => {
+    const foundGroupId = element?.dataset?.groupId || '';
+    const validGroupRegex = new RegExp(`^\\d+-${groupId}$`);
+
+    if (validGroupRegex.test(foundGroupId)) {
+      matchingElements.push(element);
+    }
+  });
+
+  return matchingElements;
+}
+
 export function removeSortingInfo(groupId: string) {
   return (groupId || '').replace(/^\d+-/, '');
 }
