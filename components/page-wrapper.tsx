be0c0cf3d6721d0a5e24c37b04c063142723c20b
Kamran Ahmed <kamranahmed.se@gmail.com>
2021-12-09 13:25:01 +0100

    Update banner colors

--- components/page-wrapper.tsx
+++ components/page-wrapper.tsx
@@ -9,7 +9,7 @@ export function PageWrapper(props: PageWrapperProps) {
   const { children } = props;
 
   return (
-    <Box bgColor='brand.hero' bgImage='url(/bg.png)' bgRepeat='no-repeat' bgSize='100%' w='100%' minH='100vh'>
+    <Box bgColor='brand.bg' bgImage='url(/bg.jpg)' bgRepeat='no-repeat' bgSize='100%' w='100%' minH='100vh'>
       { children }
     </Box>
   );
