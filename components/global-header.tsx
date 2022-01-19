b072d206ba65f1afa260960abac40dcc03ec3999
haithamleo <alkhomayasi3@gmail.com>
2022-01-19 11:31:36 +0400

    enhancement: Header pages links colors

--- components/global-header.tsx
+++ components/global-header.tsx
@@ -1,42 +1,120 @@
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { HamburgerIcon } from '@chakra-ui/icons';
-import { Box, CloseButton, Container, Flex, IconButton, Image, Link, Stack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  CloseButton,
+  Container,
+  Flex,
+  IconButton,
+  Image,
+  Link,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import RoadmapLogo from '../components/icons/roadmap.svg';
 import siteConfig from '../content/site.json';
 
-type MenuLinkProps = {
+interface MenuLinkProps {
+  active: boolean;
   text: string;
   link: string;
-};
-
-function MenuLink(props: MenuLinkProps) {
-  const { text, link } = props;
-
-  return <Link
-    borderBottomWidth={0}
-    borderBottomColor='gray.500'
-    _hover={{ textDecoration: 'none', borderBottomColor: 'white' }}
-    fontWeight={500}
-    href={link}
-  >
-    {text}
-  </Link>;
 }
 
-function DesktopMenuLinks() {
-  return (
-    <Stack d={['none', 'flex', 'flex']} shouldWrapChildren isInline spacing='15px' alignItems='center' color='gray.50'
-           fontSize='15px'>
-      <MenuLink text={'Roadmaps'} link={'/roadmaps'} />
-      <MenuLink text={'Guides'} link={'/guides'} />
-      <MenuLink text={'Videos'} link={'/watch'} />
-      <MenuLink text={'Thanks'} link={'/thanks'} />
+interface NavItemsProps {
+  mobile?: boolean;
+}
 
-      <Link ml='10px' bgGradient='linear(to-l, yellow.700, red.600)' p='7px 10px' rounded='4px'
-            _hover={{ textDecoration: 'none', bgGradient: 'linear(to-l, red.800, yellow.700)' }}
-            fontWeight={500} href={'/signup'}>Subscribe</Link>
-    </Stack>
+function MenuLink({ active, text, link }: MenuLinkProps) {
+  return (
+    <Link
+      color={active ? 'white' : 'gray.400'}
+      borderBottomWidth={0}
+      borderBottomColor="gray.500"
+      _hover={{ textDecoration: 'none', borderBottomColor: 'white' }}
+      fontWeight={500}
+      href={link}
+    >
+      {text}
+    </Link>
   );
+}
+
+const NavItems = ({ mobile }: NavItemsProps) => {
+  const router = useRouter();
+
+  const links: { name: string; href: string }[] = [
+    { name: 'Roadmaps', href: '/roadmaps' },
+    { name: 'Guides', href: '/guides' },
+    { name: 'Videos', href: '/watch' },
+    { name: 'Thanks', href: '/thanks' },
+  ];
+
+  return (
+    <>
+      {mobile ? (
+        <>
+          {links.map((link) => (
+            <MenuLink
+              active={router.pathname == link.href}
+              text={link.name}
+              link={link.href}
+            />
+          ))}
+          <Link
+            ml="10px"
+            bgGradient="linear(to-l, yellow.700, red.600)"
+            p="7px 10px"
+            rounded="4px"
+            _hover={{
+              textDecoration: 'none',
+              bgGradient: 'linear(to-l, red.800, yellow.700)',
+            }}
+            fontWeight={500}
+            href={'/signup'}
+          >
+            Subscribe
+          </Link>
+        </>
+      ) : (
+        <Stack
+          d={['none', 'flex', 'flex']}
+          shouldWrapChildren
+          isInline
+          spacing="15px"
+          alignItems="center"
+          color="gray.50"
+          fontSize="15px"
+        >
+          {links.map((link) => (
+            <MenuLink
+              active={router.pathname == link.href}
+              text={link.name}
+              link={link.href}
+            />
+          ))}
+          <Link
+            ml="10px"
+            bgGradient="linear(to-l, yellow.700, red.600)"
+            p="7px 10px"
+            rounded="4px"
+            _hover={{
+              textDecoration: 'none',
+              bgGradient: 'linear(to-l, red.800, yellow.700)',
+            }}
+            fontWeight={500}
+            href={'/signup'}
+          >
+            Subscribe
+          </Link>
+        </Stack>
+      )}
+    </>
+  );
+};
+
+function DesktopMenuLinks() {
+  return <NavItems />;
 }
 
 function MobileMenuLinks() {
@@ -45,15 +123,15 @@ function MobileMenuLinks() {
   return (
     <>
       <IconButton
-        rounded='5px'
+        rounded="5px"
         padding={0}
         aria-label={'Menu'}
         d={['block', 'none', 'none']}
-        icon={<HamburgerIcon color='white' w='25px' height='25px' />}
-        color='white'
-        cursor='pointer'
-        h='auto'
-        bg='transparent'
+        icon={<HamburgerIcon color="white" w="25px" height="25px" />}
+        color="white"
+        cursor="pointer"
+        h="auto"
+        bg="transparent"
         _hover={{ bg: 'transparent' }}
         _active={{ bg: 'transparent' }}
         _focus={{ bg: 'transparent' }}
@@ -61,25 +139,28 @@ function MobileMenuLinks() {
       />
 
       {isOpen && (
-        <Stack color='gray.100'
-               fontSize={['22px', '22px', '22px', '32px']}
-               alignItems='center'
-               justifyContent='center'
-               pos='fixed'
-               left={0}
-               right={0}
-               bottom={0}
-               top={0}
-               bg='gray.900'
-               spacing='12px'
-               zIndex={999}
+        <Stack
+          color="gray.100"
+          fontSize={['22px', '22px', '22px', '32px']}
+          alignItems="center"
+          justifyContent="center"
+          pos="fixed"
+          left={0}
+          right={0}
+          bottom={0}
+          top={0}
+          bg="gray.900"
+          spacing="12px"
+          zIndex={999}
         >
-          <Link href='/roadmaps'>Roadmaps</Link>
-          <Link href='/guides'>Guides</Link>
-          <Link href='/watch'>Videos</Link>
-          <Link href='/thanks'>Thanks</Link>
-          <Link href='/signup'>Subscribe</Link>
-          <CloseButton onClick={() => setIsOpen(false)} pos='fixed' top='40px' right='15px' size='lg' />
+          <CloseButton
+            onClick={() => setIsOpen(false)}
+            pos="fixed"
+            top="40px"
+            right="15px"
+            size="lg"
+          />
+          <NavItems mobile />
         </Stack>
       )}
     </>
@@ -87,27 +168,33 @@ function MobileMenuLinks() {
 }
 
 type GlobalHeaderProps = {
-  variant?: 'transparent' | 'solid'
+  variant?: 'transparent' | 'solid';
 };
 
 export function GlobalHeader(props: GlobalHeaderProps) {
   const { variant = 'solid' } = props;
 
   return (
-    <Box bg={variant === 'solid' ? 'gray.900' : 'transparent'} p='20px 0'>
-      <Container maxW='container.md'>
-        <Flex justifyContent='space-between' alignItems='center'>
+    <Box bg={variant === 'solid' ? 'gray.900' : 'transparent'} p="20px 0">
+      <Container maxW="container.md">
+        <Flex justifyContent="space-between" alignItems="center">
           <Box>
-            <Link w='100%'
-                  d='flex'
-                  href='/'
-                  alignItems='center'
-                  color='white'
-                  fontWeight={600}
-                  _hover={{ textDecoration: 'none' }}
-                  fontSize='18px'>
-              <RoadmapLogo style={{ height: '30px', width: '30px', marginRight: '10px' }} />
-              <Text d={['block', 'none', 'block']} as='span'>roadmap.sh</Text>
+            <Link
+              w="100%"
+              d="flex"
+              href="/"
+              alignItems="center"
+              color="white"
+              fontWeight={600}
+              _hover={{ textDecoration: 'none' }}
+              fontSize="18px"
+            >
+              <RoadmapLogo
+                style={{ height: '30px', width: '30px', marginRight: '10px' }}
+              />
+              <Text d={['block', 'none', 'block']} as="span">
+                roadmap.sh
+              </Text>
             </Link>
           </Box>
           <DesktopMenuLinks />
