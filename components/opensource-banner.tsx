aa0f98b9219ddfe925097c303a2e72a948fa8d54
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-09-05 12:29:52 +0400

    Remove sponsors information from the roadmap pages

--- components/opensource-banner.tsx
+++ components/opensource-banner.tsx
@@ -2,8 +2,8 @@ import { Box, Container, Heading, Link, Text } from '@chakra-ui/react';
 
 export function OpensourceBanner() {
   return (
-    <Box bg='white' borderTopWidth={1} pt={['45px', '45px', '70px']} pb={['60px', '60px', '90px']} textAlign='center'>
-      <Container maxW='container.md'>
+    <Box bg='white' borderTopWidth={1} py={['45px', '45px', '70px']} textAlign='center'>
+      <Container maxW='container.sm'>
         <Heading fontSize={['25px', '25px', '35px']} mb={['10px', '10px', '20px']}>Open Source</Heading>
         <Text lineHeight='26px' fontSize={['15px', '15px', '16px']} mb='20px'>The project is OpenSource,&nbsp;
           <Link
@@ -20,26 +20,9 @@ export function OpensourceBanner() {
           scrolling='0'
           width='170'
           height='30'
-          style={{ margin: 'auto', marginBottom: '30px' }}
+          style={{ margin: 'auto' }}
           title='GitHub'
         />
-
-        <Text lineHeight={['25px', '25px', '26px']} fontSize={['15px', '15px', '16px']} mb='15px'>A considerable amount of my time is spent doing unpaid
-          community work on things that I hope will help humanity in some way. Your sponsorship helps me continue to
-          produce more open-source and free educational material consumed by hundreds of thousands of developers every
-          month.</Text>
-
-        <Box>
-          <iframe
-            src='https://ghbtns.com/github-btn.html?user=kamranahmedse&type=sponsor&size=large'
-            frameBorder='0'
-            scrolling='0'
-            width='260'
-            height='30'
-            title='GitHub'
-            style={{ margin: 'auto' }}
-          />
-        </Box>
       </Container>
     </Box>
   );
