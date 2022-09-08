e0cc5d93b5a465b7221bab57e54f7942558bd709
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-09-08 17:34:17 +0400

    Add upcoming roadmap links

--- components/home/featured-roadmaps-list.tsx
+++ components/home/featured-roadmaps-list.tsx
@@ -8,6 +8,27 @@ type FeaturedRoadmapsListProps = {
 
 };
 
+export const upcomingRoadmaps = [
+  {
+    type: 'Role Based',
+    title: 'Software Architect',
+    description: 'Roadmap to become a modern Software Architect',
+    id: 'software-architect'
+  },
+  {
+    type: 'Role Based',
+    title: 'React Native',
+    description: 'Step by step guide to become a React Native Developer',
+    id: 'react-native'
+  },
+  {
+    type: 'Skill Based',
+    title: 'Design System',
+    description: 'Flowchart to help you plan and build your Design System',
+    id: 'design-system'
+  }
+];
+
 export function FeaturedRoadmapsList(props: FeaturedRoadmapsListProps) {
   const { roadmaps, title } = props;
 
@@ -15,18 +36,32 @@ export function FeaturedRoadmapsList(props: FeaturedRoadmapsListProps) {
     <>
       <Tag bg='gray.400' mb={4}>{title}</Tag>
       <SimpleGrid columns={[1, 2, 3]} spacing={['10px', '10px', '15px']} mb='40px'>
-        {roadmaps.map((roadmap: RoadmapType, counter: number) => (
-          <HomeRoadmapItem
-            isUpcoming={roadmap.isUpcoming}
-            url={`/${roadmap.id}`}
-            key={roadmap.id}
-            colorIndex={counter}
-            title={roadmap.featuredTitle}
-            isCommunity={roadmap.isCommunity}
-            isNew={roadmap.isNew}
-            subtitle={roadmap.featuredDescription}
-          />
-        ))}
+        <>
+          {roadmaps.map((roadmap: RoadmapType, counter: number) => (
+            <HomeRoadmapItem
+              isUpcoming={roadmap.isUpcoming}
+              url={`/${roadmap.id}`}
+              key={roadmap.id}
+              colorIndex={counter}
+              title={roadmap.featuredTitle}
+              isCommunity={roadmap.isCommunity}
+              isNew={roadmap.isNew}
+              subtitle={roadmap.featuredDescription}
+            />
+          ))}
+          {upcomingRoadmaps
+            .filter(roadmap => roadmap.type === title)
+            .map((roadmap, counter) => (
+              <HomeRoadmapItem
+                isUpcoming={true}
+                url={`/upcoming?id=${roadmap.id}`}
+                key={`upcoming-${roadmap.id}`}
+                colorIndex={9}
+                title={roadmap.title}
+                subtitle={roadmap.description}
+              />
+            ))}
+        </>
       </SimpleGrid>
     </>
   );
