11e581fc302592cf7bb4fee58ee312f3ae659b32
prateekme70 <114897956+prateekme70@users.noreply.github.com>
2022-10-13 12:17:22 +0530

    Update theme.ts

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,8 +3,8 @@ import { extendTheme } from '@chakra-ui/react';
 export const roadmapTheme = extendTheme({
   colors: {
     brand: {
-      bg: '#06020d',
-      hero: '#06020d',
+      bg: '#06090d',
+      hero: '#06090d',
       footer: '#0d041e'
     }
   }
