a3ab69581e139d5282e8c721a32c9e2f2895510b
Ritesh_tikare <60392084+riteshtikare@users.noreply.github.com>
2022-10-19 11:29:12 +0530

    Create newfile.ts

---
+++ styles/newfile.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const roadmapTheme = extendTheme({
+  colors: {
+    brand: {
+      bg: '#06020d',
+      hero: '#06020d',
+      footer: '#0d041e'
+    }
+  }
+});
