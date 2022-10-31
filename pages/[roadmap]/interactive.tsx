117dc4154098dbb6d3ed4c6bad142cc2eeeedb5d
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-10-31 13:13:32 +0400

    Fix pinch zoom not working on resources

--- pages/[roadmap]/interactive.tsx
+++ pages/[roadmap]/interactive.tsx
@@ -53,11 +53,6 @@ export function InteractiveRoadmapRenderer(props: RoadmapProps) {
     }
 
     function clickListener(event: MouseEvent) {
-      const viewPortMeta = document.querySelector('meta[name=viewport]');
-      if (viewPortMeta) {
-        viewPortMeta.setAttribute('content', "initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0");
-      }
-
       const targetGroup = (event?.target as HTMLElement)?.closest('g');
       const groupId = targetGroup?.dataset?.groupId;
       if (!targetGroup || !groupId) {
