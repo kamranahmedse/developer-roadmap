274eeece7aee72dddb781c96bd95717f601ca062
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-12-31 20:37:14 +0400

    Add markdown roadmap support

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}'],
-	theme: {
-		extend: {},
-		container: {
-			center: true,
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
