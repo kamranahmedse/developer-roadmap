451c36dca4149f85548786347c1b4be4a1e379c4
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-01-01 16:39:21 +0400

    Add topics page

--- src/lib/roadmap.ts
+++ src/lib/roadmap.ts
@@ -1,38 +1,71 @@
 export interface RoadmapFrontmatter {
-    id: string;
-    jsonUrl: string;
-    pdfUrl: string;
-    order: number;
-    featuredTitle: string;
-    featuredDescription: string;
+  id: string;
+  jsonUrl: string;
+  pdfUrl: string;
+  order: number;
+  featuredTitle: string;
+  featuredDescription: string;
+  title: string;
+  description: string;
+  hasTopics: boolean;
+  dimensions: {
+    width: number;
+    height: number;
+  };
+  seo: {
     title: string;
     description: string;
-    hasTopics: boolean;
-    dimensions: {
-        width: number;
-        height: number;
-    };
-    seo: {
-        title: string;
-        description: string;
-        keywords: string[];
-    };
-    relatedRoadmaps: string[];
-    sitemap: {
-        priority: number;
-        changefreq: string;
-    };
-    tags: string[];
-};
+    keywords: string[];
+  };
+  relatedRoadmaps: string[];
+  sitemap: {
+    priority: number;
+    changefreq: string;
+  };
+  tags: string[];
+}
 
 export async function getRoadmapIds() {
-    const roadmapFiles = await import.meta.glob<string>('/src/roadmaps/*/*.md', {
-        eager: true,
-    });
+  const roadmapFiles = await import.meta.glob<string>("/src/roadmaps/*/*.md", {
+    eager: true,
+  });
 
-    return Object.keys(roadmapFiles).map(filePath => {
-        const fileName = filePath.split('/').pop() || '';
+  return Object.keys(roadmapFiles).map((filePath) => {
+    const fileName = filePath.split("/").pop() || "";
 
-        return fileName.replace('.md', '');
-    });
+    return fileName.replace(".md", "");
+  });
+}
+
+export interface TopicFileType {
+    frontMatter: Record<string, string>;
+    file: string;
+    url: string;
+    Content: any;
+};
+
+export async function getTopicPathMapping() {
+  const contentFiles = await import.meta.glob<string>(
+    "/src/roadmaps/*/content/**/*.md", {
+        eager: true
+    }
+);
+
+  const mapping: Record<string, TopicFileType> = {};
+
+  Object.keys(contentFiles).forEach((filePath) => {
+    // => Sample Paths
+    // /src/roadmaps/vue/content/102-ecosystem/102-ssr/101-nuxt-js.md
+    // /src/roadmaps/vue/content/102-ecosystem/102-ssr/index.md
+    const url = filePath
+      .replace("/src/roadmaps/", "") // Remove the base `/src/roadmaps` from path
+      .replace("/content", "") // Remove the `/[roadmapName]/content`
+      .replace(/\/\d+-/g, "/") // Remove ordering info `/101-ecosystem`
+      .replace(/\/index\.md$/, "") // Make the `/index.md` to become the parent folder only
+      .replace(/\.md$/, ""); // Remove `.md` from the end of file
+
+    mapping[url] = contentFiles[filePath] as any;
+  });
+
+  return mapping;
 }
