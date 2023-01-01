50b030959003b18d36ea617c76b9f5f9b9961d18
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-01-01 18:59:38 +0400

    Handle external links in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank'}]
+    ]
+  },
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
