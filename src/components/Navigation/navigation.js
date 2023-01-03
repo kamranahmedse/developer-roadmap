b34376ce3e0292e77edbf6ea62551981446c4b1c
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-01-03 17:08:14 +0400

    Make navigation interactive

---
+++ src/components/Navigation/navigation.js
@@ -0,0 +1,44 @@
+class Navigation {
+  constructor() {
+    this.showNavigationId = 'show-mobile-navigation';
+    this.navigationId = 'mobile-navigation';
+    this.closeNavigationId = 'close-mobile-navigation';
+
+    this.init = this.init.bind(this);
+    this.onDOMLoaded = this.onDOMLoaded.bind(this);
+    this.showNavigation = this.showNavigation.bind(this);
+    this.closeNavigation = this.closeNavigation.bind(this);
+  }
+
+  get showNavigationEl() {
+    return document.getElementById(this.showNavigationId);
+  }
+
+  get navigationEl() {
+    return document.getElementById(this.navigationId);
+  }
+
+  get closeNavigationEl() {
+    return document.getElementById(this.closeNavigationId);
+  }
+
+  showNavigation() {
+    this.navigationEl.classList.toggle('hidden');
+  }
+
+  closeNavigation() {
+    this.navigationEl.classList.add('hidden');
+  }
+
+  onDOMLoaded() {
+    this.showNavigationEl.addEventListener('click', this.showNavigation);
+    this.closeNavigationEl.addEventListener('click', this.closeNavigation);
+  }
+
+  init() {
+    window.addEventListener('DOMContentLoaded', this.onDOMLoaded);
+  }
+}
+
+const navigation = new Navigation();
+navigation.init();
