ee843cc9e299435b48809ce7e218eae02a3715c5
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-01-15 15:26:58 +0400

    Add tests for roadmaps, guides and videos

--- tests/roadmap.spec.ts
+++ tests/roadmap.spec.ts
@@ -4,6 +4,12 @@ import { test, expect } from '@playwright/test';
 
 const roadmapIds = fs.readdirSync(path.join(process.cwd(), 'src/roadmaps'));
 
+test(`roadmaps`, async ({ page }) => {
+  await page.goto('/roadmaps');
+
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
 for (const roadmapId of roadmapIds) {
   test(`roadmap ${roadmapId}`, async ({ page }) => {
     await page.goto(`/${roadmapId}`);
