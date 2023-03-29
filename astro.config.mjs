5bd3d90d3c73460788b039573df979c264ff7a98
Arik Chakma <arikchangma@gmail.com>
2023-03-29 20:30:18 +0600

    feat: integrate astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,56 +5,57 @@ import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
 import { serializeSitemap, shouldIndexPage } from './sitemap.mjs';
+import preact from '@astrojs/preact';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://roadmap.sh/',
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-    },
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: '_blank',
-          rel: function (element) {
-            const href = element.properties.href;
-            const whiteListedStarts = [
-              '/',
-              '#',
-              'mailto:',
-              'https://github.com/kamranahmedse',
-              'https://thenewstack.io',
-              'https://cs.fyi',
-              'https://roadmap.sh'
-            ];
-
-            if (whiteListedStarts.some((start) => href.startsWith(start))) {
-              return [];
-            }
-
-            return 'noopener noreferrer nofollow';
-          },
-        },
-      ],
-    ],
-  },
-  build: {
-    format: 'file',
-  },
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap({
-      filter: shouldIndexPage,
-      serialize: serializeSitemap,
-    }),
-    compress({
-      css: false,
-      js: false,
-    }),
-  ],
+	site: 'https://roadmap.sh/',
+	markdown: {
+		shikiConfig: {
+			theme: 'dracula',
+		},
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: '_blank',
+					rel: function (element) {
+						const href = element.properties.href;
+						const whiteListedStarts = [
+							'/',
+							'#',
+							'mailto:',
+							'https://github.com/kamranahmedse',
+							'https://thenewstack.io',
+							'https://cs.fyi',
+							'https://roadmap.sh',
+						];
+						if (whiteListedStarts.some((start) => href.startsWith(start))) {
+							return [];
+						}
+						return 'noopener noreferrer nofollow';
+					},
+				},
+			],
+		],
+	},
+	build: {
+		format: 'file',
+	},
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		sitemap({
+			filter: shouldIndexPage,
+			serialize: serializeSitemap,
+		}),
+		compress({
+			css: false,
+			js: false,
+		}),
+		preact(),
+	],
 });
