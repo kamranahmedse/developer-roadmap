df44dad61fc738d513b120631ba8a1a0aa0e63f5
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-03-31 06:31:31 +0100

    Add login with google

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import * as jose from 'jose';
 
-export const TOKEN_COOKIE_NAME = '__timefyi_jt__';
+export const TOKEN_COOKIE_NAME = '__roadmapsh_jt__';
 export type TokenPayload = {
   id: string;
   email: string;
