8d923c4135f5dd22886e97a65635c1d0523b4b87
Arik Chakma <arikchangma@gmail.com>
2023-03-31 20:55:01 +0600

    chore: preact to astro comp

--- src/components/Login/google-login.tsx
+++ src/components/Login/google-login.tsx
@@ -1,7 +1,6 @@
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'preact/hooks';
 import { TOKEN_COOKIE_NAME } from '../../lib/utils';
-import { Spinner } from './account-nav';
 
 export default function GoogleLoginButton() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -62,6 +61,30 @@ export default function GoogleLoginButton() {
         </>
       )}
     </button>
+  );
+}
+
+function Spinner({ className }: { className?: string }) {
+  return (
+    <svg
+      className={`animate-spin" h-5 w-5 ${className}`}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="stroke-[4px] opacity-25"
+        cx={12}
+        cy={12}
+        r={10}
+        stroke="currentColor"
+      />
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
   );
 }
 
