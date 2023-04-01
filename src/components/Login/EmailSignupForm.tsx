c7fd995a86a325c7d544db767cb4b6fa7e367c98
Arik Chakma <arikchangma@gmail.com>
2023-04-01 19:38:44 +0600

    chore: email login and signup components

--- src/components/Login/EmailSignupForm.tsx
+++ src/components/Login/EmailSignupForm.tsx
@@ -3,9 +3,11 @@ import type { FunctionComponent } from 'preact';
 import { useState } from 'preact/hooks';
 import { TOKEN_COOKIE_NAME } from '../../lib/utils';
 
-const EmailLoginForm: FunctionComponent<{}> = () => {
+const EmailSignupForm: FunctionComponent<{}> = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [message, setMessage] = useState<string | null>(null);
   const [error, setError] = useState<{
     message: string;
     status: number;
@@ -16,7 +18,7 @@ const EmailLoginForm: FunctionComponent<{}> = () => {
       className="w-full"
       onSubmit={(e) => {
         e.preventDefault();
-        fetch('http://localhost:8080/v1-login', {
+        fetch('http://localhost:8080/v1-register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -24,20 +26,47 @@ const EmailLoginForm: FunctionComponent<{}> = () => {
           body: JSON.stringify({
             email,
             password,
-            // name: 'Arikko',
+            name,
           }),
-        }).then(async (res) => {
-          const json = await res.json();
-          if (res.status === 200) {
-            Cookies.set(TOKEN_COOKIE_NAME, json.token);
-            window.location.href = '/';
-          } else {
-            console.log('error', json);
-            setError(json);
-          }
-        });
+        })
+          .then(async (res) => {
+            const json = await res.json();
+            if (res.status === 200) {
+              setMessage(
+                'We have sent you an email. Please verify your account to login.'
+              );
+              setError(null);
+              console.log(json);
+            } else {
+              setMessage(null);
+              const error = new Error(json.message) as any;
+              error.status = res.status;
+              throw error;
+            }
+          })
+          .catch((err) => {
+            setMessage(null);
+            setError({
+              message: err.message,
+              status: 400,
+            });
+          });
       }}
     >
+      <label htmlFor="name" className="sr-only">
+        Name
+      </label>
+      <input
+        id="name"
+        name="name"
+        type="name"
+        autoComplete="name"
+        required
+        className="block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 shadow-sm  outline-none transition duration-150 ease-in-out placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+        placeholder="Enter you name"
+        value={name}
+        onChange={(e) => setName(String((e.target as any).value))}
+      />
       <label htmlFor="email" className="sr-only">
         Email address
       </label>
@@ -47,7 +76,7 @@ const EmailLoginForm: FunctionComponent<{}> = () => {
         type="email"
         autoComplete="email"
         required
-        className="block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 shadow-sm  outline-none transition duration-150 ease-in-out placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+        className="mt-2 block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 shadow-sm  outline-none transition duration-150 ease-in-out placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
         placeholder="Enter you email"
         value={email}
         onChange={(e) => setEmail(String((e.target as any).value))}
@@ -70,6 +99,7 @@ const EmailLoginForm: FunctionComponent<{}> = () => {
       {error && (
         <div className="mt-2 text-sm text-red-500">{error.message}</div>
       )}
+      {message && <div className="mt-2 text-sm text-green-500">{message}</div>}
 
       <button
         type="submit"
@@ -81,4 +111,4 @@ const EmailLoginForm: FunctionComponent<{}> = () => {
   );
 };
 
-export default EmailLoginForm;
+export default EmailSignupForm;
