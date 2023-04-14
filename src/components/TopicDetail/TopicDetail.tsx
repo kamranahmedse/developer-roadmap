ec9f836a1f84fdd7f5585f1af91f442633af6f34
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-04-14 20:56:24 +0100

    Fix check icon

--- src/components/TopicDetail/TopicDetail.tsx
+++ src/components/TopicDetail/TopicDetail.tsx
@@ -197,7 +197,7 @@ export function TopicDetail() {
                   className="inline-flex items-center rounded-md bg-green-600 p-1 px-2 text-sm text-white hover:bg-green-700"
                   onClick={() => setIsActive(false)}
                 >
-                  <img alt="Check" src={CheckIcon} />
+                  <img alt="Check" class='w-3' src={CheckIcon} />
                   <span className="ml-2">Mark as Done</span>
                 </button>
               )}
@@ -219,7 +219,7 @@ export function TopicDetail() {
                       className="inline-flex items-center rounded-md border border-green-600 bg-green-600 p-1 px-2 text-sm text-white hover:bg-green-700"
                       onClick={() => toggleMarkTopicDone(true)}
                     >
-                      <img alt="Check" class="h-4 w-4" src={CheckIcon} />
+                      <img alt="Check" class="w-3" src={CheckIcon} />
                       <span className="ml-2">Mark as Done</span>
                     </button>
                   )}
