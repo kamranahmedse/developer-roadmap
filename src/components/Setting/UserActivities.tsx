fef79dd203a78f95618c6f1c51bd1753dbe8e178
Arik Chakma <arikchangma@gmail.com>
2023-05-16 01:20:04 +0600

    chore: Loading Skeleton

--- src/components/Setting/UserActivities.tsx
+++ src/components/Setting/UserActivities.tsx
@@ -69,52 +69,125 @@ export default function UserActivities() {
   }, [loadActivities]);
 
   return (
+    <>
+      {isLoading ? (
+        <UserActivitiesSkeleton />
+      ) : (
+        <div>
+          <div className="pl-0 pt-4 md:p-10 md:pb-0 md:pr-0">
+            <div className="divider-gray-300 grid divide-y rounded border border-gray-300 sm:grid-cols-3 sm:divide-x sm:divide-y-0 sm:divide-gray-300 sm:border-none">
+              <div className="flex items-center gap-3 p-2 sm:p-0">
+                <img src={CheckDarkIcon} alt="done" className="h-6 w-6" />
+                <div>
+                  <h3 className="text-xs text-gray-600">Topics Completed</h3>
+                  <p className="mt-1 text-lg font-bold leading-none">
+                    {data?.topicsCompleted || 0}{' '}
+                    <span className="text-xs font-normal text-gray-400">
+                      +{data?.topicsCompletedToday || 0} today
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3 p-2 sm:p-0 sm:pl-2">
+                <img
+                  src={ProgressDarkIcon}
+                  alt="learning"
+                  className="h-6 w-6"
+                />
+                <div>
+                  <h3 className="text-xs text-gray-600">Currently Learning</h3>
+                  <p className="mt-1 text-lg font-bold leading-none">
+                    {data?.topicsLearning || 0}
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3 p-2 sm:p-0 sm:pl-2">
+                <img src={StarDarkIcon} alt="star" className="h-6 w-6" />
+                <div>
+                  <h3 className="text-xs text-gray-600">Learning Streak</h3>
+                  <p className="mt-1 text-lg font-bold leading-none">
+                    {data?.streak || 0}{' '}
+                    <span className="text-xs font-normal text-gray-400">
+                      {data?.streak === 1 ? 'day' : 'days'}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="pl-0 pt-4 md:p-10 md:pr-0 md:pt-8">
+            <div>
+              <h4 className="text-2xl font-medium">Roadmaps</h4>
+              <div className="mt-2 grid gap-2 sm:grid-cols-2">
+                {data?.learning.roadmap.map((item) => (
+                  <LearningProgress
+                    resource={item}
+                    key={item._id?.toString()}
+                  />
+                ))}
+
+                {data?.learning.roadmap.length === 0 && (
+                  <p>No roadmaps found. Start learning now!</p>
+                )}
+              </div>
+            </div>
+
+            <div className="mt-5">
+              <h4 className="text-2xl font-medium">Best Practices</h4>
+              <div className="mt-2 grid gap-2 sm:grid-cols-2">
+                {data?.learning.bestPractice.map((item) => (
+                  <LearningProgress
+                    resource={item}
+                    key={item._id?.toString()}
+                  />
+                ))}
+
+                {data?.learning.bestPractice.length === 0 && (
+                  <p>No best practices found. Start learning now!</p>
+                )}
+              </div>
+            </div>
+
+            <h3 className="mt-8 text-2xl font-medium">Recent Activities</h3>
+            <ul className="mt-4 flex flex-col gap-2">
+              {data?.activities.map((activity) => (
+                <li>
+                  <UserActivity activity={activity} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+function UserActivitiesSkeleton() {
+  return (
     <div>
       <div className="pl-0 pt-4 md:p-10 md:pb-0 md:pr-0">
         <div className="divider-gray-300 grid divide-y rounded border border-gray-300 sm:grid-cols-3 sm:divide-x sm:divide-y-0 sm:divide-gray-300 sm:border-none">
           <div className="flex items-center gap-3 p-2 sm:p-0">
-            <img src={CheckDarkIcon} alt="done" className="h-6 w-6" />
+            <div className="mt-1 h-6 w-6 rounded-full bg-gray-300" />
             <div>
-              <h3 className="text-xs text-gray-600">Topics Completed</h3>
-              {isLoading ? (
-                <div className="mt-1 h-[18px] w-10 rounded bg-gray-300" />
-              ) : (
-                <p className="mt-1 text-lg font-bold leading-none">
-                  {data?.topicsCompleted || 0}{' '}
-                  <span className="text-xs font-normal text-gray-400">
-                    +{data?.topicsCompletedToday || 0} today
-                  </span>
-                </p>
-              )}
+              <div className="mt-1 h-3 w-10 rounded bg-gray-300" />
+              <div className="mt-1 h-[18px] w-10 rounded bg-gray-300" />
             </div>
           </div>
           <div className="flex items-center gap-3 p-2 sm:p-0 sm:pl-2">
-            <img src={ProgressDarkIcon} alt="learning" className="h-6 w-6" />
+            <div className="mt-1 h-6 w-6 rounded-full bg-gray-300" />
             <div>
-              <h3 className="text-xs text-gray-600">Currently Learning</h3>
-              {isLoading ? (
-                <div className="mt-1 h-[18px] w-10 rounded bg-gray-300" />
-              ) : (
-                <p className="mt-1 text-lg font-bold leading-none">
-                  {data?.topicsLearning || 0}
-                </p>
-              )}
+              <div className="mt-1 h-3 w-10 rounded bg-gray-300" />
+              <div className="mt-1 h-[18px] w-10 rounded bg-gray-300" />
             </div>
           </div>
           <div className="flex items-center gap-3 p-2 sm:p-0 sm:pl-2">
-            <img src={StarDarkIcon} alt="star" className="h-6 w-6" />
+            <div className="mt-1 h-6 w-6 rounded-full bg-gray-300" />
             <div>
-              <h3 className="text-xs text-gray-600">Learning Streak</h3>
-              {isLoading ? (
-                <div className="mt-1 h-[18px] w-10 rounded bg-gray-300" />
-              ) : (
-                <p className="mt-1 text-lg font-bold leading-none">
-                  {data?.streak || 0}{' '}
-                  <span className="text-xs font-normal text-gray-400">
-                    {data?.streak === 1 ? 'day' : 'days'}
-                  </span>
-                </p>
-              )}
+              <div className="mt-1 h-3 w-10 rounded bg-gray-300" />
+              <div className="mt-1 h-[18px] w-10 rounded bg-gray-300" />
             </div>
           </div>
         </div>
@@ -122,70 +195,39 @@ export default function UserActivities() {
 
       <div className="pl-0 pt-4 md:p-10 md:pr-0 md:pt-8">
         <div>
-          <h4 className="text-2xl font-medium">Roadmaps</h4>
+          <div className="h-8 w-1/3 rounded bg-gray-300" />
           <div className="mt-2 grid gap-2 sm:grid-cols-2">
-            {isLoading && (
-              <>
-                <LearningProgressSkeleton />
-                <LearningProgressSkeleton />
-                <LearningProgressSkeleton />
-                <LearningProgressSkeleton />
-              </>
-            )}
-            {data?.learning.roadmap.map((item) => (
-              <LearningProgress resource={item} key={item._id?.toString()} />
-            ))}
-
-            {data?.learning.roadmap.length === 0 && (
-              <p>No roadmaps found. Start learning now!</p>
-            )}
+            <LearningProgressSkeleton />
+            <LearningProgressSkeleton />
+            <LearningProgressSkeleton />
+            <LearningProgressSkeleton />
           </div>
         </div>
 
         <div className="mt-5">
-          <h4 className="text-2xl font-medium">Best Practices</h4>
+          <div className="h-8 w-1/3 rounded bg-gray-300" />
           <div className="mt-2 grid gap-2 sm:grid-cols-2">
-            {isLoading && (
-              <>
-                <LearningProgressSkeleton />
-                <LearningProgressSkeleton />
-                <LearningProgressSkeleton />
-                <LearningProgressSkeleton />
-              </>
-            )}
-            {data?.learning.bestPractice.map((item) => (
-              <LearningProgress resource={item} key={item._id?.toString()} />
-            ))}
-
-            {data?.learning.bestPractice.length === 0 && (
-              <p>No best practices found. Start learning now!</p>
-            )}
+            <LearningProgressSkeleton />
+            <LearningProgressSkeleton />
+            <LearningProgressSkeleton />
+            <LearningProgressSkeleton />
           </div>
         </div>
 
-        <h3 className="mt-8 text-2xl font-medium">Recent Activities</h3>
+        <div className="mt-8 h-8 w-1/3 rounded bg-gray-300" />
         <ul className="mt-4 flex flex-col gap-2">
-          {isLoading && (
-            <>
-              <li>
-                <UserActivitySkeleton />
-              </li>
-              <li>
-                <UserActivitySkeleton />
-              </li>
-              <li>
-                <UserActivitySkeleton />
-              </li>
-              <li>
-                <UserActivitySkeleton />
-              </li>
-            </>
-          )}
-          {data?.activities.map((activity) => (
-            <li>
-              <UserActivity activity={activity} />
-            </li>
-          ))}
+          <li>
+            <UserActivitySkeleton />
+          </li>
+          <li>
+            <UserActivitySkeleton />
+          </li>
+          <li>
+            <UserActivitySkeleton />
+          </li>
+          <li>
+            <UserActivitySkeleton />
+          </li>
         </ul>
       </div>
     </div>
