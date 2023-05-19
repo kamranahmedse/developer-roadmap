430085cbb75f58d24889bd382e6de2bcadd15355
Arik Chakma <arikchangma@gmail.com>
2023-05-19 15:55:39 +0600

    chore: outside click and focus trapping

--- src/components/Command/Command.tsx
+++ src/components/Command/Command.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import { createPortal } from 'preact/compat';
 import CommandSearch from './CommandSearch';
+import { useOutsideClick } from '../../hooks/use-outside-click';
 
 export default function Command() {
+  const commandModalRef = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   useEffect(() => {
@@ -16,10 +18,37 @@ export default function Command() {
     };
 
     window.addEventListener('keydown', openCommand);
-
     return () => {
       window.removeEventListener('keydown', openCommand);
     };
+  }, [isOpen]);
+
+  function handleTrapFocusKeydown(e: KeyboardEvent) {
+    const focusableElements = commandModalRef.current?.querySelectorAll(
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+    ) as NodeListOf<HTMLElement>;
+
+    const firstFocusableElement = focusableElements[0];
+    const lastFocusableElement =
+      focusableElements[focusableElements.length - 1];
+
+    if (e.key === 'Tab') {
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          e.preventDefault();
+          lastFocusableElement.focus();
+        }
+      } else {
+        if (document.activeElement === lastFocusableElement) {
+          e.preventDefault();
+          firstFocusableElement.focus();
+        }
+      }
+    }
+  }
+
+  useOutsideClick(commandModalRef, () => {
+    setIsOpen(false);
   });
 
   return (
@@ -28,11 +57,13 @@ export default function Command() {
         createPortal(
           <>
             <div className="fixed inset-0 h-screen w-screen">
+              <div className="absolute inset-0 bg-black opacity-50" />
               <div
-                className="absolute inset-0 bg-black opacity-50"
-                onClick={() => setIsOpen(false)}
-              />
-              <div className="absolute left-1/2 top-20 z-50 w-[50vw] -translate-x-1/2">
+                ref={commandModalRef}
+                onKeyDown={handleTrapFocusKeydown}
+                className="absolute left-1/2 top-20 z-50 w-[50vw] -translate-x-1/2"
+                tabIndex={-1}
+              >
                 <CommandSearch />
               </div>
             </div>
