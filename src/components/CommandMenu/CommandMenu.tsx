a2119d488d13c0242ef45b9f243eed47b0c735cd
Arik Chakma <arikchangma@gmail.com>
2023-05-20 14:48:46 +0600

    chore: command menu navigation

--- src/components/CommandMenu/CommandMenu.tsx
+++ src/components/CommandMenu/CommandMenu.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from 'preact/hooks';
-import { RefObject, createPortal } from 'preact/compat';
+import { createPortal } from 'preact/compat';
 import CommandSearch from './CommandSearch';
 import { useOutsideClick } from '../../hooks/use-outside-click';
 
 export default function Command() {
   const commandModalRef = useRef<HTMLDivElement>(null);
+  const initialTriggerRef = useRef<HTMLButtonElement>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [activeElement, setActiveElement] = useState<HTMLElement | null>(null);
   const [activeElementIndex, setActiveElementIndex] = useState<number>(0);
@@ -16,6 +17,7 @@ export default function Command() {
         setIsOpen(true);
       } else if (e.key === 'Escape') {
         setIsOpen(false);
+        initialTriggerRef.current?.focus();
       }
     };
 
@@ -62,7 +64,7 @@ export default function Command() {
 
     const selectedElement = focusableElements[index];
     selectedElement.ariaSelected = 'true';
-
+    
     for (const element of focusableElements) {
       if (element !== selectedElement) {
         element.removeAttribute('aria-selected');
@@ -120,12 +122,30 @@ export default function Command() {
   };
 
   useEffect(() => {
-    // activeElement?.focus();
     console.log(activeElement);
   }, [activeElement]);
 
   return (
     <>
+      <button
+        onClick={() => setIsOpen(true)}
+        ref={initialTriggerRef}
+        className="group flex h-6 w-6 items-center justify-center rounded p-1 hover:bg-gray-50/20"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          class="h-4 w-4 stroke-gray-400 group-hover:stroke-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </button>
       {isOpen &&
         createPortal(
           <>
