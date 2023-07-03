a27d41b7182d63a89053ba6b70725444e2328a10
Arik Chakma <arikchangma@gmail.com>
2023-07-04 04:29:43 +0600

    chore: team sidebar

---
+++ src/components/TeamSidebar.tsx
@@ -0,0 +1,13 @@
+export function TeamSidebar() {
+  return (
+    <div className="h-full w-48 bg-gray-100 px-4 py-0 md:py-10">
+      <div>
+        <ul className="space-y-2">
+          <li>Progress</li>
+          <li>Members</li>
+          <li>Settings</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
