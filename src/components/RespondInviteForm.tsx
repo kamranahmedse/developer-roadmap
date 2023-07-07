ad23bb1bf88faebbd9bc64b70ccf0695841ead66
Arik Chakma <arikchangma@gmail.com>
2023-07-07 21:41:43 +0600

    chore: respond invite

---
+++ src/components/RespondInviteForm.tsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from 'preact/hooks';
+import { useParams } from '../hooks/use-params';
+import { httpGet, httpPatch } from '../lib/http';
+
+export function RespondInviteForm() {
+  const [containerOpacity, setContainerOpacity] = useState(0);
+  const [isPreparing, setIsPreparing] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const { inviteId } = useParams<{ inviteId: string }>();
+
+  // async function getInvitation() {
+  //   const { response, error } = await httpGet(
+  //     `${import.meta.env.PUBLIC_API_URL}/v1-get-invitation/${inviteId}`
+  //   );
+  //   if (error || !response) {
+  //     setError(error?.message || 'Something went wrong')
+  //     return;
+  //   }
+
+  //   const teamName = document.getElementById('team-name');
+  //   if (teamName) {
+  //     teamName.innerText = response.name;
+  //   }
+  // }
+
+  function showRespondContainer() {
+    const respondEl = document.getElementById('respond-invite')!;
+    if (!respondEl) {
+      return;
+    }
+
+    respondEl.classList.add('opacity-0');
+    setTimeout(() => {
+      respondEl.parentElement?.removeChild(respondEl);
+      setIsPreparing(false);
+
+      setTimeout(() => {
+        setContainerOpacity(100);
+      }, 50);
+    }, 0);
+  }
+
+  async function respondInvitation(status: 'accept' | 'reject') {
+    setIsLoading(true);
+    setError('');
+    const { response, error } = await httpPatch<{ teamId: string }>(
+      `${import.meta.env.PUBLIC_API_URL}/v1-respond-invite/${inviteId}`, {
+      status
+    });
+    if (error || !response) {
+      setError(error?.message || 'Something went wrong')
+      setIsLoading(false)
+      return;
+    }
+
+    console.log(response);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    showRespondContainer()
+    setIsPreparing(false);
+  }, [])
+
+  if (isPreparing) {
+    return null
+  }
+
+  return (
+    <div className={`max-w-md mx-auto w-full transition-opacity duration-500 opacity-${containerOpacity}`}>
+      <div className="flex items-center gap-2 w-full">
+        <button
+          type="button"
+          disabled={isLoading}
+          onClick={() => respondInvitation('accept')}
+          className="flex-grow cursor-pointer rounded-lg bg-gray-200 px-3 py-2 text-center"
+        >
+          {isLoading ? 'Please wait ..' : 'Accept'}
+        </button>
+        <button
+          type="button"
+          disabled={isLoading}
+          onClick={() => respondInvitation('reject')}
+          className="flex-grow cursor-pointer rounded-lg bg-red-500 px-3 py-2 text-white disabled:opacity-40"
+        >
+          {isLoading ? 'Please wait ..' : 'Reject'}
+        </button>
+      </div>
+
+      {error && (
+        <p className="mt-2 rounded-lg bg-red-100 p-2 text-red-700">{error}</p>
+      )}
+    </div>
+  );
+}
--- src/directives/client-authenticated.mjs
+++ src/directives/client-authenticated.mjs
@@ -1,5 +1,9 @@
+/**
+ * @type {import('astro').ClientDirective}
+ */
 export default async (load, opts) => {
-  const isAuthenticated = document.cookie.toString().indexOf('__roadmapsh_jt__') !== -1;
+  const isAuthenticated =
+    document.cookie.toString().indexOf('__roadmapsh_jt__') !== -1;
   if (isAuthenticated) {
     const hydrate = await load();
     await hydrate();
