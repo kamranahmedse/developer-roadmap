3f763051bb0da0e2151a771760ddea8ebbac2e4a
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-07-11 19:12:50 +0100

    Add spinner to team dropdown

--- src/components/TeamDropdown/TeamDropdown.tsx
+++ src/components/TeamDropdown/TeamDropdown.tsx
@@ -1,25 +1,35 @@
-import {useEffect, useRef, useState} from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import ChevronDown from '../../icons/dropdown.svg';
 import { httpGet } from '../../lib/http';
 import { useTeamId } from '../../hooks/use-team-id';
 import type { TeamDocument } from '../CreateTeam/CreateTeamForm';
 import { useAuth } from '../../hooks/use-auth';
-import {useOutsideClick} from "../../hooks/use-outside-click";
+import { useOutsideClick } from '../../hooks/use-outside-click';
+import { Spinner } from '../ReactIcons/Spinner';
 
 export function TeamDropdown() {
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   const [showDropdown, setShowDropdown] = useState(false);
   const [teamList, setTeamList] = useState<TeamDocument[]>([]);
-  const { teamId } = useTeamId();
-  const user = useAuth()
 
-  const selectedAvatar = teamList.find((team) => team._id === teamId)?.avatar || user?.avatar || '';
+  const { teamId } = useTeamId();
+  const user = useAuth();
+
+  const selectedAvatar = teamId
+    ? teamList.find((team) => team._id === teamId)?.avatar
+    : user?.avatar;
+
+  const selectedLabel = teamId
+    ? teamList.find((team) => team._id === teamId)?.name
+    : 'Personal';
 
   useOutsideClick(dropdownRef, () => {
     setShowDropdown(false);
   });
 
-  async function getAllTeam() {
+  async function getAllTeams() {
     const { response, error } = await httpGet<{
       data: TeamDocument[];
     }>(`${import.meta.env.PUBLIC_API_URL}/v1-get-user-all-team`);
@@ -32,7 +42,10 @@ export function TeamDropdown() {
   }
 
   useEffect(() => {
-    getAllTeam().then();
+    setIsLoading(true);
+    getAllTeams().finally(() => {
+      setIsLoading(false);
+    });
   }, []);
 
   return (
@@ -42,24 +55,37 @@ export function TeamDropdown() {
         onClick={() => setShowDropdown(!showDropdown)}
       >
         <div className="flex items-center gap-2">
-          <img src={
-            selectedAvatar
-              ? `${import.meta.env.PUBLIC_AVATAR_BASE_URL}/${selectedAvatar}`
-              : '/images/default-avatar.png'
-          } alt='' className="w-4 h-4 rounded-full" />
+          {isLoading && <Spinner className="h-4 w-4" isDualRing={false} />}
+          {!isLoading && (
+            <img
+              src={
+                selectedAvatar
+                  ? `${
+                      import.meta.env.PUBLIC_AVATAR_BASE_URL
+                    }/${selectedAvatar}`
+                  : '/images/default-avatar.png'
+              }
+              alt=""
+              className="h-4 w-4 rounded-full"
+            />
+          )}
           <span className="truncate">
-            {teamList.find((team) => team._id === teamId)?.name || 'Personal'}
+            {!isLoading && selectedLabel }
+            {isLoading && 'Loading ..'}
           </span>
         </div>
         <img alt={'show dropdown'} src={ChevronDown} className="h-4 w-4" />
-      </button >
+      </button>
 
       {showDropdown && (
-        <div ref={dropdownRef} className="absolute top-full z-50 mt-2 w-full rounded-md bg-slate-800 px-2 py-2 text-white shadow-md">
+        <div
+          ref={dropdownRef}
+          className="absolute top-full z-50 mt-2 w-full rounded-md bg-slate-800 px-2 py-2 text-white shadow-md"
+        >
           <ul>
             <li>
               <a
-                className="flex w-full cursor-pointer items-center rounded p-2 text-sm font-medium text-slate-100 hover:bg-slate-700 gap-2 text-sm truncate"
+                className="flex w-full cursor-pointer items-center gap-2 truncate rounded p-2 text-sm text-sm font-medium text-slate-100 hover:bg-slate-700"
                 href="/account"
               >
                 <span className="truncate">Personal Account</span>
@@ -68,7 +94,7 @@ export function TeamDropdown() {
             {teamList.map((team) => (
               <li>
                 <a
-                  className="flex w-full cursor-pointer items-center rounded p-2 text-sm font-medium text-slate-100 hover:bg-slate-700 gap-2"
+                  className="flex w-full cursor-pointer items-center gap-2 rounded p-2 text-sm font-medium text-slate-100 hover:bg-slate-700"
                   href={`/team/progress?t=${team._id}`}
                 >
                   <span className="truncate">{team.name}</span>
@@ -84,8 +110,7 @@ export function TeamDropdown() {
             <span>New Team</span>
           </a>
         </div>
-      )
-      }
-    </div >
+      )}
+    </div>
   );
 }
