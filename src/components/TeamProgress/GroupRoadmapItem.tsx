ae14d560a1389fea00ecc7b9a4a60be17e732628
Arik Chakma <arikchangma@gmail.com>
2023-07-20 05:59:54 +0600

    fix: progress details

--- src/components/TeamProgress/GroupRoadmapItem.tsx
+++ src/components/TeamProgress/GroupRoadmapItem.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'preact/hooks';
-import type { GroupByRoadmap } from './TeamProgressPage';
+import type { GroupByRoadmap, TeamMember } from './TeamProgressPage';
+import { MemberProgressModal } from './MemberProgressModal';
+import { getUrlParams } from '../../lib/browser';
 
 type GroupRoadmapItemProps = {
   roadmap: GroupByRoadmap;
@@ -7,64 +9,84 @@ type GroupRoadmapItemProps = {
 
 export function GroupRoadmapItem(props: GroupRoadmapItemProps) {
   const { members, resourceTitle } = props.roadmap;
+  const { t: teamId } = getUrlParams();
 
   const [showAll, setShowAll] = useState(false);
+  const [detailResourceId, setDetailResourceId] = useState<string | null>(null);
+  const [selectedMember, setSelectedMember] = useState<TeamMember | null>(null);
 
   return (
-    <div className="flex h-full min-h-[270px] flex-col rounded-md border">
-      <div className="flex items-center gap-3 border-b p-3">
-        <div className="flex min-w-0 flex-grow flex-col">
-          <h3 className="truncate font-medium">{resourceTitle}</h3>
+    <>
+      {detailResourceId && (
+        <MemberProgressModal
+          member={selectedMember!}
+          teamId={teamId}
+          resourceId={detailResourceId}
+          resourceType={'roadmap'}
+          onClose={() => {
+            setDetailResourceId(null);
+            setSelectedMember(null);
+          }}
+        />
+      )}
+      <div className="flex h-full min-h-[270px] flex-col rounded-md border">
+        <div className="flex items-center gap-3 border-b p-3">
+          <div className="flex min-w-0 flex-grow flex-col">
+            <h3 className="truncate font-medium">{resourceTitle}</h3>
+          </div>
+        </div>
+        <div className="relative flex grow flex-col space-y-2 p-3">
+          {(showAll ? members : members.slice(0, 4)).map((member) => {
+            if (!member.progress) return null;
+            return (
+              <button
+                className="group relative w-full overflow-hidden rounded-md border p-2 hover:border-gray-300 hover:text-black focus:outline-none"
+                key={member?.member._id}
+                onClick={() => {
+                  setDetailResourceId(member?.progress?.resourceId!);
+                  setSelectedMember(member.member);
+                }}
+              >
+                <span className="relative z-10 flex items-center justify-between text-sm">
+                  <span>{member?.member?.name}</span>
+                  <span className="text-xs text-gray-400">
+                    {member?.progress?.done} / {member?.progress?.total}
+                  </span>
+                </span>
+                <span
+                  className="absolute inset-0 bg-gray-100 group-hover:bg-gray-200"
+                  style={{
+                    width: `${(member?.progress?.done / member?.progress?.total) * 100
+                      }%`,
+                  }}
+                />
+              </button>
+            );
+          })}
+
+          {members.length > 4 && !showAll && (
+            <button
+              onClick={() => setShowAll(true)}
+              className={'text-sm text-gray-400 underline'}
+            >
+              + {members.length - 4} more
+            </button>
+          )}
+
+          {showAll && (
+            <button
+              onClick={() => setShowAll(false)}
+              className={'text-sm text-gray-400 underline'}
+            >
+              - Show less
+            </button>
+          )}
+
+          {members.length === 0 && (
+            <div className="text-sm text-gray-500">No progress</div>
+          )}
         </div>
       </div>
-      <div className="relative flex grow flex-col space-y-2 p-3">
-        {(showAll ? members : members.slice(0, 4)).map((member) => {
-          if (!member.progress) return null;
-          return (
-            <div
-              className="group relative overflow-hidden rounded-md border p-2 hover:border-gray-300 hover:text-black focus:outline-none"
-              key={member?.member._id}
-            >
-              <span className="relative z-10 flex items-center justify-between text-sm">
-                <span>{member?.member?.name}</span>
-                <span className="text-xs text-gray-400">
-                  {member?.progress?.done} / {member?.progress?.total}
-                </span>
-              </span>
-              <span
-                className="absolute inset-0 bg-gray-100 group-hover:bg-gray-200"
-                style={{
-                  width: `${
-                    (member?.progress?.done / member?.progress?.total) * 100
-                  }%`,
-                }}
-              />
-            </div>
-          );
-        })}
-
-        {members.length > 4 && !showAll && (
-          <button
-            onClick={() => setShowAll(true)}
-            className={'text-sm text-gray-400 underline'}
-          >
-            + {members.length - 4} more
-          </button>
-        )}
-
-        {showAll && (
-          <button
-            onClick={() => setShowAll(false)}
-            className={'text-sm text-gray-400 underline'}
-          >
-            - Show less
-          </button>
-        )}
-
-        {members.length === 0 && (
-          <div className="text-sm text-gray-500">No progress</div>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
