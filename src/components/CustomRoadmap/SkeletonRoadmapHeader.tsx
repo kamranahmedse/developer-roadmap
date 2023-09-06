fad6a2e6ecb1dad2ce922ae690c9d8f253c12c08
Arik Chakma <arikchangma@gmail.com>
2023-09-06 18:17:33 +0600

    fix: skeleton loading width

--- src/components/CustomRoadmap/SkeletonRoadmapHeader.tsx
+++ src/components/CustomRoadmap/SkeletonRoadmapHeader.tsx
@@ -8,8 +8,8 @@ export function SkeletonRoadmapHeader() {
         </div>
 
         <div className="flex justify-between gap-2 sm:gap-0">
-          <div className="h-7 w-24 animate-pulse rounded-md bg-gray-300 sm:h-8" />
-          <div className="h-7 w-24 animate-pulse rounded-md bg-gray-300 sm:h-8" />
+          <div className="h-7 w-32 animate-pulse rounded-md bg-gray-300 sm:h-8" />
+          <div className="h-7 w-32 animate-pulse rounded-md bg-gray-300 sm:h-8" />
         </div>
 
         <div className="mb-0 mt-4 rounded-md border-0 sm:-mb-[65px] sm:mt-7 sm:border">
