6a5edfc3ec64f964c616ecaaa9f712c5e1a8504d
Sai Chaitanya Raparti <chaitanya.raparti@ezetap.com>
2023-10-07 10:13:36 +0530

    adding return

--- src/components/UpdateProfile/UploadProfilePicture.tsx
+++ src/components/UpdateProfile/UploadProfilePicture.tsx
@@ -111,6 +111,7 @@ export default function UploadProfilePicture(props: UploadProfilePictureProps) {
     if (error || !response) {
       setError(error?.message || 'Something went wrong');
       setIsLoading(false);
+      return;
     }
 
     // Logout user if token is invalid
@@ -119,7 +120,7 @@ export default function UploadProfilePicture(props: UploadProfilePictureProps) {
       window.location.reload();
       return;
     }
-    
+
     window.location.reload();
     return;
   };
