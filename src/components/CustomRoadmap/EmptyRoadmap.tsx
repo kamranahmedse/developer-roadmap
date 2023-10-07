476e1c9f23bc586a9036e05e2255dd6f777cfe24
Arik Chakma <arikchangma@gmail.com>
2023-10-07 23:57:27 +0600

    Add Edit Roadmap permission

--- src/components/CustomRoadmap/EmptyRoadmap.tsx
+++ src/components/CustomRoadmap/EmptyRoadmap.tsx
@@ -2,10 +2,11 @@ import { CircleSlash } from 'lucide-react';
 
 type EmptyRoadmapProps = {
   roadmapId: string;
+  canManage: boolean;
 };
 
 export function EmptyRoadmap(props: EmptyRoadmapProps) {
-  const { roadmapId } = props;
+  const { roadmapId, canManage } = props;
   const editUrl = `${import.meta.env.PUBLIC_EDITOR_APP_URL}/${roadmapId}`;
 
   return (
@@ -14,12 +15,14 @@ export function EmptyRoadmap(props: EmptyRoadmapProps) {
         <CircleSlash className="mx-auto h-20 w-20 text-gray-400" />
         <h3 className="mt-2">This roadmap is currently empty.</h3>
 
-        <a
-          href={editUrl}
-          className="mt-4 rounded-lg bg-black px-4 py-2 font-medium text-white hover:bg-gray-900"
-        >
-          Edit Roadmap
-        </a>
+        {canManage && (
+          <a
+            href={editUrl}
+            className="mt-4 rounded-lg bg-black px-4 py-2 font-medium text-white hover:bg-gray-900"
+          >
+            Edit Roadmap
+          </a>
+        )}
       </div>
     </div>
   );
