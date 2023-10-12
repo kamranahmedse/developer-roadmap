29f6298e2fc0e555edd428c2203b172decc14c23
Arik Chakma <arikchangma@gmail.com>
2023-10-12 23:39:00 +0600

    Implement utils

--- editor/utils/roadmap.ts
+++ editor/utils/roadmap.ts
@@ -4,10 +4,18 @@ export const INITIAL_DESKTOP_ZOOM = 1;
 export const MINIMUM_DESKTOP_ZOOM = 0.75;
 
 export function centerRoadmap(options: any) {
+  console.warn('centerRoadmap is not implemented');
+  console.warn('run the following command to generate the renderer:');
+  console.warn('> npm run generate-renderer');
+
   return;
 }
 
 export function calculateDimensions(options: any) {
+  console.warn('calculateDimensions is not implemented');
+  console.warn('run the following command to generate the renderer:');
+  console.warn('> npm run generate-renderer');
+
   return {
     x: 0,
     y: 0,
