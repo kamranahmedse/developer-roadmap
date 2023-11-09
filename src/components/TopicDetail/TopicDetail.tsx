34907e86bd1b637312dea462cf1d64f9591bcd7e
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-11-09 04:48:38 +0000

    Refactor topic detail

--- src/components/TopicDetail/TopicDetail.tsx
+++ src/components/TopicDetail/TopicDetail.tsx
@@ -159,13 +159,7 @@ export function TopicDetail(props: TopicDetailProps) {
         }
         let topicHtml = '';
         if (!isCustomResource) {
-          // It's full HTML with page body, head etc.
-          // We only need the inner HTML of the #main-content
-          const node = new DOMParser().parseFromString(
-            response as string,
-            'text/html',
-          );
-          topicHtml = response;
+          topicHtml = response as string;
         } else {
           setLinks((response as RoadmapContentDocument)?.links || []);
           setTopicTitle((response as RoadmapContentDocument)?.title || '');
