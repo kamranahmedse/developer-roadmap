4fcf1f6d5043c8c3938449d4b66e4c46aa513fba
Arik Chakma <arikchangma@gmail.com>
2023-12-28 08:24:20 +0600

    fix: redirect to the error page

--- src/pages/[roadmapId]/index.json.ts
+++ src/pages/[roadmapId]/index.json.ts
@@ -3,14 +3,31 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = async function ({ params, request, props }) {
   const { roadmapId } = params;
 
-  const roadmapJson = await import(
-    `../../data/roadmaps/${roadmapId}/${roadmapId}.json`
-  );
+  try {
+    const roadmapJson = await import(
+      `../../data/roadmaps/${roadmapId}/${roadmapId}.json`
+    );
 
-  return new Response(JSON.stringify(roadmapJson), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+    return new Response(JSON.stringify(roadmapJson), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: {
+          message: 'Roadmap not found',
+        },
+      }),
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  }
 };
