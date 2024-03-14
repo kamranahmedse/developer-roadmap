36e8e6051be711148bd2dd9caf051a52514ca375
Arik Chakma <arikchangma@gmail.com>
2024-03-14 14:06:02 +0600

    fix: open graph query params

--- src/lib/open-graph.ts
+++ src/lib/open-graph.ts
@@ -1,19 +1,10 @@
-type GuideOpenGraphParams = {
-  type: 'guide';
-  authorName: string;
-  authorAvatar?: string;
-  title: string;
-  description: string;
+type RoadmapOpenGraphQuery = {
+  type: 'roadmaps' | 'guides' | 'best-practices';
+  variant?: 'default' | 'image';
+  resourceId?: string;
+  guideId?: string;
 };
 
-type RoadmapOpenGraphParams = {
-  type: 'roadmap';
-  title: string;
-  description: string;
-};
-
-export function getOpenGraphImageUrl(
-  params: GuideOpenGraphParams | RoadmapOpenGraphParams,
-) {
+export function getOpenGraphImageUrl(params: RoadmapOpenGraphQuery) {
   return `${import.meta.env.PUBLIC_API_URL}/v1-open-graph?${new URLSearchParams(params)}`;
 }
