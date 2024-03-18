dd2ce9b9883c95c2c06387900fa7b25a7ca127aa
Arik Chakma <arikchangma@gmail.com>
2024-03-18 21:03:14 +0600

    wip: implement success screen

--- src/components/AuthenticationFlow/TriggerVerifyEmail.tsx
+++ src/components/AuthenticationFlow/TriggerVerifyEmail.tsx
@@ -4,14 +4,19 @@ import { setAuthToken } from '../../lib/jwt';
 import { Spinner } from '../ReactIcons/Spinner';
 import { ErrorIcon2 } from '../ReactIcons/ErrorIcon2';
 import { getUrlParams } from '../../lib/browser';
+import { CheckIcon } from '../ReactIcons/CheckIcon';
 
 export function TriggerVerifyEmail() {
   const { code } = getUrlParams() as { code: string };
-  const [isLoading, setIsLoading] = useState(true);
+
+  // const [isLoading, setIsLoading] = useState(true);
+  const [status, setStatus] = useState<'loading' | 'error' | 'success'>(
+    'loading',
+  );
   const [error, setError] = useState('');
 
   const triggerVerify = (code: string) => {
-    setIsLoading(true);
+    setStatus('loading');
 
     httpPatch<{ token: string }>(
       `${import.meta.env.PUBLIC_API_URL}/v1-verify-new-email/${code}`,
@@ -20,29 +25,44 @@ export function TriggerVerifyEmail() {
       .then(({ response, error }) => {
         if (!response?.token) {
           setError(error?.message || 'Something went wrong. Please try again.');
-          setIsLoading(false);
+          setStatus('error');
 
           return;
         }
 
         setAuthToken(response.token);
-        window.location.href = '/';
+        setStatus('success');
       })
       .catch((err) => {
-        setIsLoading(false);
+        setStatus('error');
         setError('Something went wrong. Please try again.');
       });
   };
 
   useEffect(() => {
     if (!code) {
-      setIsLoading(false);
+      setStatus('error');
       setError('Something went wrong. Please try again later.');
       return;
     }
 
     triggerVerify(code);
   }, [code]);
+
+  const isLoading = status === 'loading';
+  if (status === 'success') {
+    return (
+      <div className="mx-auto flex max-w-md flex-col items-center pt-0 sm:pt-12">
+        <CheckIcon additionalClasses={'h-16 w-16 opacity-100'} />
+        <h2 className="mb-1 mt-4 text-center text-xl font-semibold sm:mb-3 sm:mt-4 sm:text-2xl">
+          Email Update Successful
+        </h2>
+        <p className="text-sm sm:text-base">
+          Your email has been changed successfully. Happy learning!
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className="mx-auto flex max-w-md flex-col items-center pt-0 sm:pt-12">
--- src/components/UpdateEmail/UpdateEmailForm.tsx
+++ src/components/UpdateEmail/UpdateEmailForm.tsx
@@ -2,6 +2,7 @@ import { type FormEvent, useState } from 'react';
 import { httpPatch } from '../../lib/http';
 import { pageProgressMessage } from '../../stores/page';
 import { useToast } from '../../hooks/use-toast';
+import { cn } from '../../lib/classname';
 
 type UpdateEmailFormProps = {
   authProvider: string;
@@ -28,6 +29,9 @@ export function UpdateEmailForm(props: UpdateEmailFormProps) {
 
   const handleSentVerificationCode = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!newEmail || !newEmail.includes('@') || isSubmitted) {
+      return;
+    }
 
     setIsLoading(true);
     pageProgressMessage.set('Sending verification code');
@@ -122,56 +126,61 @@ export function UpdateEmailForm(props: UpdateEmailFormProps) {
         </p>
       </div>
 
-      {!isSubmitted ? (
-        <form onSubmit={handleSentVerificationCode} className="space-y-4">
-          <div className="flex w-full flex-col">
-            <label
-              htmlFor="current-email"
-              className="text-sm leading-none text-slate-500"
-            >
-              Current Email
-            </label>
-            <input
-              type="email"
-              name="current-email"
-              id="current-email"
-              autoComplete="current-email"
-              className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
-              required
-              disabled
-              value={currentEmail}
-            />
-          </div>
-          <div className="flex w-full flex-col">
-            <label
-              htmlFor="new-email"
-              className="text-sm leading-none text-slate-500"
-            >
-              New Email
-            </label>
-            <input
-              type="email"
-              name="new-email"
-              id="new-email"
-              autoComplete={'new-email'}
-              className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
-              required
-              placeholder="Enter new email"
-              value={newEmail}
-              onChange={(e) => setNewEmail(e.target.value)}
-            />
-          </div>
-
-          <button
-            type="submit"
-            disabled={isLoading || !newEmail || !newEmail.includes('@')}
-            className="inline-flex w-full items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400"
+      <form onSubmit={handleSentVerificationCode} className="space-y-4">
+        <div className="flex w-full flex-col">
+          <label
+            htmlFor="current-email"
+            className="text-sm leading-none text-slate-500"
           >
-            {isLoading ? 'Please wait...' : 'Send Verification Code'}
-          </button>
-        </form>
-      ) : (
-        <div className="space-y-4">
+            Current Email
+          </label>
+          <input
+            type="email"
+            name="current-email"
+            id="current-email"
+            autoComplete="current-email"
+            className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+            required
+            disabled
+            value={currentEmail}
+          />
+        </div>
+        <div className="flex w-full flex-col">
+          <label
+            htmlFor="new-email"
+            className="text-sm leading-none text-slate-500"
+          >
+            New Email
+          </label>
+          <input
+            type="email"
+            name="new-email"
+            id="new-email"
+            autoComplete={'new-email'}
+            className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+            required
+            placeholder="Enter new email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+            disabled={isSubmitted}
+          />
+        </div>
+
+        <button
+          type="submit"
+          disabled={
+            isLoading || !newEmail || !newEmail.includes('@') || isSubmitted
+          }
+          className={cn(
+            'inline-flex w-full items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400',
+            isSubmitted && 'hidden',
+          )}
+        >
+          {isLoading ? 'Please wait...' : 'Send Verification Code'}
+        </button>
+      </form>
+      {isSubmitted && (
+        <div className="mt-4 space-y-4">
           <p className="text-gray-400">
             A verification code has been sent to your email. Please check your
             inbox and verify your new email.
@@ -180,14 +189,14 @@ export function UpdateEmailForm(props: UpdateEmailFormProps) {
             <button
               onClick={handleCancelEmailVerification}
               disabled={isLoading}
-              className="inline-flex items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400"
+              className="inline-flex items-center justify-center rounded-lg border-2 border-black p-2 py-3 text-sm font-medium outline-none transition-colors hover:bg-black hover:text-white focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400"
             >
               {isLoading ? 'Please wait...' : 'Cancel'}
             </button>
             <button
               onClick={handleResendVerificationCode}
               disabled={isLoading || isResendDone}
-              className="inline-flex items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400"
+              className="inline-flex items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none hover:opacity-90 focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400"
             >
               {isLoading ? 'Please wait...' : 'Resend Verification Code'}
             </button>
