3a1b896eb2d937854e0280774710d7a5e33c191c
Arik Chakma <arikchangma@gmail.com>
2024-04-26 05:12:10 +0600

    Add default opengraph on error (#5552)

--- src/lib/open-graph.ts
+++ src/lib/open-graph.ts
@@ -6,3 +6,8 @@ type RoadmapOpenGraphQuery = {
 export function getOpenGraphImageUrl(params: RoadmapOpenGraphQuery) {
   return `${import.meta.env.DEV ? 'http://localhost:3000' : 'https://roadmap.sh'}/og-images/${params.group}/${params.resourceId}.png`;
 }
+
+export async function getDefaultOpenGraphImageBuffer() {
+  const defaultImageUrl = `${import.meta.env.DEV ? 'http://localhost:3000' : 'https://roadmap.sh'}/images/og-img.png`;
+  return fetch(defaultImageUrl).then((response) => response.arrayBuffer());
+}
