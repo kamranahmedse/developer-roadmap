8763ef6b42e1fb19acc2e94034dc0df4652ab1a7
Kamran Ahmed <kamranahmed.se@gmail.com>
2024-05-13 16:58:52 +0100

    Update UI for onboarding dropdown

--- src/components/Navigation/AccountDropdownList.tsx
+++ src/components/Navigation/AccountDropdownList.tsx
@@ -6,11 +6,15 @@ import {
   SquareUserRound,
   User2,
   Users2,
+  Handshake,
 } from 'lucide-react';
 import { logout } from './navigation';
 import { CreateRoadmapModal } from '../CustomRoadmap/CreateRoadmap/CreateRoadmapModal.tsx';
 import { useState } from 'react';
 import { cn } from '../../lib/classname.ts';
+import { NotificationIndicator } from './NotificationIndicator.tsx';
+import { Spinner } from '../ReactIcons/Spinner.tsx';
+import { CheckIcon } from '../ReactIcons/CheckIcon.tsx';
 
 type AccountDropdownListProps = {
   onCreateRoadmap: () => void;
@@ -36,24 +40,27 @@ export function AccountDropdownList(props: AccountDropdownListProps) {
   return (
     <ul>
       {shouldShowOnboardingStatus && (
-        <li className="px-1">
+        <li className="mb-1 px-1">
           <button
             className={cn(
-              'flex h-9 w-full items-center rounded border py-1 pl-3 pr-2 text-sm font-medium text-slate-100 hover:opacity-80',
+              'flex h-9 w-full items-center rounded py-1 pl-3 pr-2 text-sm font-medium text-slate-100 hover:opacity-80',
               isConfigLoading
-                ? 'striped-loader border-slate-700'
+                ? 'striped-loader-lighter flex border-slate-800 opacity-70'
                 : 'border-slate-600 bg-slate-700',
             )}
             onClick={onOnboardingClick}
             disabled={isConfigLoading}
           >
+            <NotificationIndicator className="-left-0.5 -top-0.5" />
+
             {isConfigLoading ? (
-              ''
+              <></>
             ) : (
               <>
+                <Handshake className="mr-2 h-4 w-4 text-slate-400 group-hover:text-white" />
                 <span>Onboarding</span>
-                <span className="ml-auto flex items-center gap-1.5 text-xs">
-                  {doneConfigCount}&nbsp;/&nbsp;{onboardingConfigCount}
+                <span className="ml-auto flex items-center gap-1.5 text-xs text-slate-400">
+                  {doneConfigCount} of {onboardingConfigCount}
                 </span>
               </>
             )}
--- src/components/Navigation/NotificationIndicator.tsx
+++ src/components/Navigation/NotificationIndicator.tsx
@@ -1,6 +1,18 @@
-export function NotificationIndicator() {
+import { cn } from '../../lib/classname.ts';
+
+type NotificationIndicatorProps = {
+  className?: string;
+};
+export function NotificationIndicator(props: NotificationIndicatorProps) {
+  const { className = '' } = props;
+
   return (
-    <span className="absolute right-0 -top-1 text-xs uppercase tracking-wider h-3 w-3">
+    <span
+      className={cn(
+        'absolute -top-1 right-0 h-3 w-3 text-xs uppercase tracking-wider',
+        className,
+      )}
+    >
       <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
       <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
     </span>
