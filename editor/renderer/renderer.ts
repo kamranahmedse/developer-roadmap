7f96b69f7a4a4ed9dfacba7e340fbb1e705212a8
Arik Chakma <arikchangma@gmail.com>
2024-05-18 01:18:19 +0600

    wip

---
+++ editor/renderer/renderer.ts
@@ -0,0 +1,5 @@
+export function renderFlowJSON(data: any, options?: any) {
+  console.warn("renderFlowJSON is not implemented");
+  console.warn("run the following command to generate the renderer:");
+  console.warn("> npm run generate-renderer");
+}
