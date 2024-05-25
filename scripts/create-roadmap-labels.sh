fbec23cab96317c85ca0069f9ee350ce6294a899
nazrinrazak <nazrinrazakbmi@gmail.com>
2024-05-25 22:55:37 +0800

    Improved user interface and code readability

--- scripts/create-roadmap-labels.sh
+++ scripts/create-roadmap-labels.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+# Function to create GitHub label with a given name, color, and description
+create_label() {
+    local name=$1
+    local color=$2
+    local description=$3
+    gh label create "$name" --color "$color" --description "$description"
+}
+
 # Get all the folder names inside src/data/roadmaps
 roadmap_ids=$(ls src/data/roadmaps)
 
-# Create a label for each roadmap name on GitHub issues using gh cli
-for roadmap_id in $roadmap_ids
-do
-  random_color=$(openssl rand -hex 3)
-  gh label create "$roadmap_id" --color $random_color --description "$roadmap_id label"
+# Loop through each roadmap to create a GitHub label
+for roadmap_id in $roadmap_ids; do
+    # Generate a random color for the label
+    random_color=$(openssl rand -hex 3)
+
+    # Create label for the roadmap
+    create_label "$roadmap_id" "$random_color" "Label for $roadmap_id"
 done
