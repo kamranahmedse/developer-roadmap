05b549b09636cb88a66b1b7df05ebc5068fb93ba
Arik Chakma <arikchangma@gmail.com>
2024-07-29 18:42:03 +0600

    refactor: roadmap discover page

--- src/components/DiscoverRoadmaps/DiscoverRoadmaps.tsx
+++ src/components/DiscoverRoadmaps/DiscoverRoadmaps.tsx
@@ -4,29 +4,133 @@ import { Pagination } from '../Pagination/Pagination';
 import { SearchRoadmap } from './SearchRoadmap';
 import { EmptyDiscoverRoadmaps } from './EmptyDiscoverRoadmaps';
 import { Rating } from '../Rating/Rating';
+import { useEffect, useState } from 'react';
+import { deleteUrlParam, getUrlParams, setUrlParams } from '../../lib/browser';
+import { LoadingRoadmaps } from '../ExploreAIRoadmap/LoadingRoadmaps';
+import { httpGet } from '../../lib/http';
+import { useToast } from '../../hooks/use-toast';
+import { DiscoverRoadmapSorting } from './DiscoverRoadmapSorting';
 
-type DiscoverRoadmapsProps = {
-  searchParams: string;
-  roadmapsResponse: ListShowcaseRoadmapResponse;
+type DiscoverRoadmapsProps = {};
+
+export type SortByValues = 'rating' | '-rating' | 'createdAt' | '-createdAt';
+
+type QueryParams = {
+  q?: string;
+  s?: SortByValues;
+  p?: string;
+};
+
+type PageState = {
+  searchTerm: string;
+  sortBy: SortByValues;
+  currentPage: number;
 };
 
 export function DiscoverRoadmaps(props: DiscoverRoadmapsProps) {
-  const { roadmapsResponse, searchParams: defaultSearchparams } = props;
+  const toast = useToast();
+
+  const [pageState, setPageState] = useState<PageState>({
+    searchTerm: '',
+    sortBy: 'createdAt',
+    currentPage: 0,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [roadmapsResponse, setRoadmapsResponse] =
+    useState<ListShowcaseRoadmapResponse | null>(null);
+
+  useEffect(() => {
+    const queryParams = getUrlParams() as QueryParams;
+
+    setPageState({
+      searchTerm: queryParams.q || '',
+      sortBy: queryParams.s || 'createdAt',
+      currentPage: +(queryParams.p || '1'),
+    });
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (!pageState.currentPage) {
+      return;
+    }
+
+    // only set the URL params if the user modified anything
+    if (
+      pageState.currentPage !== 1 ||
+      pageState.searchTerm !== '' ||
+      pageState.sortBy !== 'createdAt'
+    ) {
+      setUrlParams({
+        q: pageState.searchTerm,
+        s: pageState.sortBy,
+        p: String(pageState.currentPage),
+      });
+    } else {
+      deleteUrlParam('q');
+      deleteUrlParam('s');
+      deleteUrlParam('p');
+    }
+
+    loadAIRoadmaps(
+      pageState.currentPage,
+      pageState.searchTerm,
+      pageState.sortBy,
+    ).finally(() => {
+      setIsLoading(false);
+    });
+  }, [pageState]);
+
+  const loadAIRoadmaps = async (
+    currPage: number = 1,
+    searchTerm: string = '',
+    sortBy: SortByValues = 'createdAt',
+  ) => {
+    const { response, error } = await httpGet<ListShowcaseRoadmapResponse>(
+      `${import.meta.env.PUBLIC_API_URL}/v1-list-showcase-roadmap`,
+      {
+        currPage,
+        ...(searchTerm && { term: searchTerm }),
+        ...(sortBy && { sortBy }),
+      },
+    );
+
+    if (error || !response) {
+      toast.error(error?.message || 'Something went wrong');
+      return;
+    }
+
+    setRoadmapsResponse(response);
+  };
 
   const roadmaps = roadmapsResponse?.data || [];
 
-  const searchParams = new URLSearchParams(defaultSearchparams);
-  const titleQuery = searchParams.get('q') || '';
+  const loadingIndicator = isLoading && <LoadingRoadmaps />;
 
   return (
     <section className="container mx-auto py-3 sm:py-6">
-      <SearchRoadmap
-        total={roadmapsResponse?.totalCount || 0}
-        value={titleQuery}
-      />
+      <div className="mb-3.5 flex items-stretch justify-between gap-2.5">
+        <SearchRoadmap
+          total={roadmapsResponse?.totalCount || 0}
+          value={pageState.searchTerm}
+          isLoading={isLoading}
+          onValueChange={(value) => {}}
+        />
 
-      {roadmaps.length === 0 && <EmptyDiscoverRoadmaps />}
-      {roadmaps.length > 0 && (
+        <DiscoverRoadmapSorting
+          sortBy={pageState.sortBy}
+          onSortChange={(sortBy) => {
+            setPageState({
+              ...pageState,
+              sortBy,
+            });
+          }}
+        />
+      </div>
+
+      {loadingIndicator}
+      {roadmaps.length === 0 && !isLoading && <EmptyDiscoverRoadmaps />}
+      {roadmaps.length > 0 && !isLoading && (
         <>
           <ul className="mb-4 grid grid-cols-1 items-stretch gap-2 sm:grid-cols-2 lg:grid-cols-3">
             {roadmaps.map((roadmap) => {
@@ -75,13 +179,10 @@ export function DiscoverRoadmaps(props: DiscoverRoadmapsProps) {
             perPage={roadmapsResponse?.perPage || 0}
             totalCount={roadmapsResponse?.totalCount || 0}
             onPageChange={(page) => {
-              const newSearchParams = new URLSearchParams();
-              if (titleQuery) {
-                newSearchParams.set('q', titleQuery);
-              }
-
-              newSearchParams.set('currPage', page.toString());
-              window.location.href = `/discover?${newSearchParams.toString()}`;
+              setPageState({
+                ...pageState,
+                currentPage: page,
+              });
             }}
           />
         </>
