82fbb1235eb18d18b2760ec9af70aeaafde5b7fa
Kamran Ahmed <kamranahmed.se@gmail.com>
2024-08-18 00:37:33 +0100

    Update feature announcement

--- src/components/FeatureAnnouncement.tsx
+++ src/components/FeatureAnnouncement.tsx
@@ -1,19 +1,20 @@
-type AIAnnouncementProps = {};
+type FeatureAnnouncementProps = {};
 
-export function FeatureAnnouncement(props: AIAnnouncementProps) {
+export function FeatureAnnouncement(props: FeatureAnnouncementProps) {
   return (
     <a
       className="rounded-md border border-dashed border-purple-600 px-3 py-1.5 text-purple-400 transition-colors hover:border-purple-400 hover:text-purple-200"
-      href="/community"
+      href="https://www.youtube.com/watch?v=9lS3slfJ0x0"
+      target='_blank'
     >
       <span className="relative -top-[1px] mr-1 text-xs font-semibold uppercase text-white">
         New
       </span>{' '}
       <span className={'hidden sm:inline'}>
-        Explore community made roadmaps
+        Practice your skills with projects
       </span>
       <span className={'inline text-sm sm:hidden'}>
-        Community roadmaps explorer!
+        Build projects to skill up
       </span>
     </a>
   );
