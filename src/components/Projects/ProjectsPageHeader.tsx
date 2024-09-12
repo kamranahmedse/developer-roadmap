bcc85dcebe2097b86db3009999305d34910f1aac
Kamran Ahmed <kamranahmed.se@gmail.com>
2024-09-12 13:08:38 +0100

    Update projects header

--- src/components/Projects/ProjectsPageHeader.tsx
+++ src/components/Projects/ProjectsPageHeader.tsx
@@ -7,7 +7,7 @@ export function ProjectsPageHeader() {
       <div className="container">
         <div className="flex flex-col items-start bg-white sm:items-center">
           <h1 className="text-2xl font-bold sm:text-5xl">Project Ideas</h1>
-          <p className="mt-1 text-sm sm:my-3 sm:text-lg">
+          <p className="mt-1 text-sm sm:mt-4 sm:text-lg">
             Browse the ever-growing list of projects ideas and solutions.
           </p>
         </div>
