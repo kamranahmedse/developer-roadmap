8b0c53675075bef3333558f6e6ae7be07220a837
Arik Chakma <arikchangma@gmail.com>
2024-10-07 17:47:54 +0600

    feat: team dashboard (#7213)
    * fix: add team roadmaps
    
    * feat: implement add member
    
    * feat: separate team dashboard page
    
    * UI changes for team dashboard
    
    * Add team activity dashboard
    
    ---------
    
    Co-authored-by: Kamran Ahmed <kamranahmed.se@gmail.com>

--- src/components/TeamActivity/TeamActivityItem.tsx
+++ src/components/TeamActivity/TeamActivityItem.tsx
@@ -102,7 +102,7 @@ export function TeamActivityItem(props: TeamActivityItemProps) {
     return (
       <li
         key={user._id}
-        className="flex flex-wrap items-center gap-1 rounded-md border px-2 py-2.5 text-sm"
+        className="flex flex-wrap items-center gap-1 rounded-md border px-2 py-2.5 text-sm bg-white"
       >
         {actionType === 'in_progress' && (
           <>
@@ -158,7 +158,7 @@ export function TeamActivityItem(props: TeamActivityItemProps) {
   const activityLimit = showAll ? activities.length : 5;
 
   return (
-    <li key={user._id} className="overflow-hidden rounded-md border">
+    <li key={user._id} className="overflow-hidden bg-white rounded-md border">
       <h3 className="flex flex-wrap items-center gap-1 bg-gray-100 px-2 py-2.5 text-sm">
         {username} has {activities.length} updates in {uniqueResourcesCount}
         &nbsp;resource(s)
--- src/components/TeamActivity/TeamEmptyStream.tsx
+++ src/components/TeamActivity/TeamEmptyStream.tsx
@@ -10,7 +10,7 @@ export function TeamEmptyStream(props: TeamActivityItemProps) {
   return (
     <div className="rounded-md">
       <div className="flex flex-col items-center p-7 text-center sm:p-14">
-        <ListTodo className="mb-4 h-[60px] w-[60px] opacity-10 sm:h-[60px] sm:w-[60px]" />
+        <ListTodo className="mb-4 h-14 w-14 opacity-10" />
 
         <h2 className="text-lg font-semibold sm:text-lg">No Activity</h2>
         <p className="my-1 max-w-[400px] text-balance text-sm text-gray-500 sm:my-2 sm:text-base">
