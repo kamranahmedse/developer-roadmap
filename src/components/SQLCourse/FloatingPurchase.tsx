9890675ca2d2affa2a018711c5251519e7449236
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-01-22 22:44:45 +0000

    Floating purchase indicatorg

--- src/components/SQLCourse/FloatingPurchase.tsx
+++ src/components/SQLCourse/FloatingPurchase.tsx
@@ -1,8 +1,28 @@
 import { ArrowRightIcon } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { cn } from '../../lib/classname';
 
 export function FloatingPurchase() {
+  const [isHidden, setIsHidden] = useState(true);
+
+  useEffect(() => {
+    function onScroll() {
+      setIsHidden(window.scrollY < 400);
+    }
+
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-[999] flex items-center justify-center p-4">
+    <div
+      className={cn(
+        'fixed bottom-5 left-0 right-0 z-[5] flex items-center justify-center transition-all duration-200 ease-out',
+        {
+          'pointer-events-none -bottom-10 opacity-0': isHidden,
+        },
+      )}
+    >
       <div className="flex w-full max-w-[800px] items-center justify-between rounded-2xl bg-yellow-950 p-5 shadow-lg ring-1 ring-yellow-500/40">
         <div className="flex flex-col">
           <h2 className="mb-1 text-xl font-medium text-white">
