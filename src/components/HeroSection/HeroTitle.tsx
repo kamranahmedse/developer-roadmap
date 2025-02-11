d7f74b34a6289568f72c6b7177a8cf504886cd31
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-02-11 01:33:40 +0000

    Fix spacing

--- src/components/HeroSection/HeroTitle.tsx
+++ src/components/HeroSection/HeroTitle.tsx
@@ -25,7 +25,7 @@ export function HeroTitle(props: HeroTitleProps) {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-3">
-        <p className="flex items-center text-sm text-gray-400">
+        <p className="flex items-center gap-0.5 text-sm text-gray-400">
           {!isLoading && icon}
           {isLoading && (
             <span className="mr-1.5">
