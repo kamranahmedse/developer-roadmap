eb5e5fadccc9af1556b2105689d1ae81ed846ce5
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-03-17 13:27:55 +0000

    Add python FAQs

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -24,27 +24,6 @@ const md = new MarkdownIt({
 
 export function markdownToHtml(markdown: string, isInline = true): string {
   try {
-    // Solution to open links in new tab in markdown
-    // otherwise default behaviour is to open in same tab
-    //
-    // SOURCE: https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
-    //
-    const defaultRender =
-      md.renderer.rules.link_open ||
-      // @ts-ignore
-      function (tokens, idx, options, env, self) {
-        return self.renderToken(tokens, idx, options);
-      };
-
-    // @ts-ignore
-    md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-      // Add a new `target` attribute, or replace the value of the existing one.
-      tokens[idx].attrSet('target', '_blank');
-
-      // Pass the token to the default renderer.
-      return defaultRender(tokens, idx, options, env, self);
-    };
-
     if (isInline) {
       return md.renderInline(markdown);
     } else {
