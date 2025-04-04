a5eb5231cb5bde31968361f409a6e5f6df718c1c
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-04-04 21:03:12 +0100

    Make deployment script accept pat

--- scripts/generate-renderer.sh
+++ scripts/generate-renderer.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-if [ -n "$GH_PAT" ]; then
-  LATEST_COMMIT_HASH=$(git ls-remote "https://${GH_PAT}@github.com/roadmapsh/web-draw.git" refs/heads/main | awk '{print $1}')
-  echo "Using commit hash: $LATEST_COMMIT_HASH"
-  pnpm add "https://${GH_PAT}@github.com/roadmapsh/web-draw#${LATEST_COMMIT_HASH}&path:packages/editor"
-else
-  LATEST_COMMIT_HASH=$(git ls-remote git@github.com:roadmapsh/web-draw.git refs/heads/main | awk '{print $1}')
-  echo "Using commit hash: $LATEST_COMMIT_HASH"
-  pnpm add "github:roadmapsh/web-draw#${LATEST_COMMIT_HASH}&path:packages/editor"
-fi
+LATEST_COMMIT_HASH=$(git ls-remote git@github.com:roadmapsh/web-draw.git refs/heads/main | awk '{print $1}')
+
+echo "Using commit hash: $LATEST_COMMIT_HASH"
+pnpm add "github:roadmapsh/web-draw#${LATEST_COMMIT_HASH}&path:packages/editor"
