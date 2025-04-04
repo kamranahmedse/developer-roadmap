63b6d471a2340751f7e0c3acb33004c62be6ddb5
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-04-04 20:41:35 +0100

    Update generate-renderer

--- scripts/generate-renderer.sh
+++ scripts/generate-renderer.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Fetch the latest commit hash from the GitHub repo
-LATEST_COMMIT_HASH=$(git ls-remote https://github.com/roadmapsh/web-draw-v2.git refs/heads/main | awk '{print $1}')
+LATEST_COMMIT_HASH=$(git ls-remote git@github.com:roadmapsh/web-draw.git refs/heads/main | awk '{print $1}')
 
 echo "Latest commit hash: $LATEST_COMMIT_HASH"
 
