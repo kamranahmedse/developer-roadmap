f95ef58a93b3e6d9616a47d203fe15eadc601348
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-04-09 16:31:55 +0100

    Update AI tutor sidebar

---
+++ src/components/ReactIcons/AITutorLogo.tsx
@@ -0,0 +1,36 @@
+import type { SVGProps } from 'react';
+
+type AITutorLogoProps = SVGProps<SVGSVGElement>;
+
+export function AITutorLogo(props: AITutorLogoProps) {
+  return (
+    <svg
+      viewBox="0 0 310 248"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect width="310" height="247.211" fill="black" />
+      <path
+        d="M205.179 45.1641H263.851V201.278H205.179V45.1641Z"
+        fill="white"
+      />
+      <path
+        d="M45.1641 45.1743H104.598L104.598 202.048H45.1641L45.1641 45.1743Z"
+        fill="white"
+      />
+      <path
+        d="M160.984 45.1743V103.716L45.1641 103.716L45.1641 45.1743H160.984Z"
+        fill="white"
+      />
+      <path
+        d="M125.171 45.1743H184.605V201.284H125.171V45.1743Z"
+        fill="white"
+      />
+      <path
+        d="M159.841 131.85V173.88L63.8324 173.88L63.8324 131.85H159.841Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
