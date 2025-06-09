7af779d6ba336057293bb24940d439f0d0d63dee
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-06-09 17:31:17 +0100

    AI Document content

--- src/components/GenerateDocument/AIDocumentContent.tsx
+++ src/components/GenerateDocument/AIDocumentContent.tsx
@@ -1,4 +1,9 @@
-import { markdownToHtml } from '../../lib/markdown';
+import { useEffect, useState } from 'react';
+import {
+  markdownToHtml,
+  markdownToHtmlWithHighlighting,
+} from '../../lib/markdown';
+import './AIDocumentContent.css';
 
 type AIDocumentContentProps = {
   document: string;
@@ -7,12 +12,23 @@ type AIDocumentContentProps = {
 export function AIDocumentContent(props: AIDocumentContentProps) {
   const { document } = props;
 
-  const html = markdownToHtml(document, false);
+  const [html, setHtml] = useState('');
+
+  useEffect(() => {
+    const html = markdownToHtmlWithHighlighting(document)
+      .then((html) => {
+        setHtml(html);
+      })
+      .catch((e) => {
+        console.error(e);
+        return markdownToHtml(document, false);
+      });
+  }, [document]);
 
   return (
     <div className="mx-auto w-full max-w-4xl">
       <div
-        className="course-content prose prose-lg prose-headings:mb-3 prose-headings:mt-8 prose-blockquote:font-normal prose-pre:rounded-2xl prose-pre:text-lg prose-li:my-1 prose-thead:border-zinc-800 prose-tr:border-zinc-800 max-lg:prose-h2:mt-3 max-lg:prose-h2:text-lg max-lg:prose-h3:text-base max-lg:prose-pre:px-3 max-lg:prose-pre:text-sm mt-8 max-w-full text-black max-lg:mt-4 max-lg:text-base"
+        className="course-content [&>h1]:text-balance prose prose-lg prose-headings:mb-3 prose-headings:mt-8 prose-blockquote:font-normal prose-pre:rounded-2xl prose-pre:text-lg prose-li:my-1 prose-thead:border-zinc-800 prose-tr:border-zinc-800 max-lg:prose-h2:mt-3 max-lg:prose-h2:text-lg max-lg:prose-h3:text-base max-lg:prose-pre:px-3 max-lg:prose-pre:text-sm mt-8 max-w-full text-black max-lg:mt-4 max-lg:text-base"
         dangerouslySetInnerHTML={{ __html: html }}
       />
     </div>
