1a6cd543ba3971f14bec18646ea6611a6780c5c4
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-07-11 15:58:31 +0100

    Make profile noindex if no roadmaps

--- src/components/UserPublicProfile/UserPublicProfileHeader.tsx
+++ src/components/UserPublicProfile/UserPublicProfileHeader.tsx
@@ -80,6 +80,7 @@ export function UserLink(props: UserLinkProps) {
       target="_blank"
       href={href}
       className="flex h-6 w-6 items-center justify-center rounded-md border"
+      rel="nofollow noopener noreferrer"
     >
       <Icon className="h-3.5 w-3.5 shrink-0 stroke-2" />
     </a>
