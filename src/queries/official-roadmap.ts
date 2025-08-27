580e764097b4fab7fffe7e3c908ae11a22267272
Arik Chakma <arikchangma@gmail.com>
2025-08-27 18:38:41 +0600

    chore: update roadmap json endpoint

--- src/queries/official-roadmap.ts
+++ src/queries/official-roadmap.ts
@@ -2,13 +2,59 @@ import { queryOptions } from '@tanstack/react-query';
 import { httpGet } from '../lib/query-http';
 import type { Node, Edge } from '@roadmapsh/editor';
 
+export const allowedOfficialRoadmapType = ['skill', 'role'] as const;
+export type AllowedOfficialRoadmapType =
+  (typeof allowedOfficialRoadmapType)[number];
+
+export const allowedOfficialRoadmapQuestionType = ['faq', 'main'] as const;
+export type AllowedOfficialRoadmapQuestionType =
+  (typeof allowedOfficialRoadmapQuestionType)[number];
+
+export type OfficialRoadmapQuestion = {
+  _id: string;
+  type: AllowedOfficialRoadmapQuestionType;
+  title: string;
+  // Tiptap JSON Content
+  description: any;
+};
+
 export interface OfficialRoadmapDocument {
   _id: string;
-  title: string;
-  description?: string;
+  order: number;
+
+  title: {
+    card: string;
+    page: string;
+  };
+  description: string;
+
   slug: string;
   nodes: Node[];
   edges: Edge[];
+
+  draft: {
+    nodes: Node[];
+    edges: Edge[];
+  };
+
+  seo: {
+    title: string;
+    description: string;
+    keywords: string[];
+  };
+  partner?: {
+    description: string;
+    linkText: string;
+    link: string;
+  };
+  type: AllowedOfficialRoadmapType;
+  dimensions?: {
+    height: number;
+    width: number;
+  };
+
+  questions?: OfficialRoadmapQuestion[];
+  relatedRoadmaps?: string[];
 
   createdAt: Date;
   updatedAt: Date;
