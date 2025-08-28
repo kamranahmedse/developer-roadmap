70700b13135e5e7d0d9213b6a95f26b6fd74f3d4
Arik Chakma <arikchangma@gmail.com>
2025-08-28 12:32:54 +0600

    feat: roadmap main page

---
+++ src/components/FAQs/FAQs.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import type { OfficialRoadmapQuestion } from '../../queries/official-roadmap';
+import { Question } from './Question';
+import { guideRenderer } from '../../lib/guide-renderer';
+
+type FAQsProps = {
+  faqs: OfficialRoadmapQuestion[];
+};
+
+export function FAQs(props: FAQsProps) {
+  const { faqs } = props;
+
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+
+  return (
+    <div className="mt-8 border-t bg-gray-100">
+      <div className="container">
+        <div className="relative -top-5 flex justify-between">
+          <h2 className="rounded-md border bg-white px-3 py-1 text-sm font-medium sm:text-base">
+            Frequently Asked Questions
+          </h2>
+        </div>
+
+        <div className="flex flex-col gap-1 pb-14">
+          {faqs.map((faq, questionIndex) => (
+            <Question
+              key={faq._id}
+              isActive={questionIndex === activeQuestionIndex}
+              question={faq.title}
+              onClick={() => setActiveQuestionIndex(questionIndex)}
+            >
+              <div className="text-md rounded-br-md rounded-bl-md border-t border-t-gray-300 bg-gray-100 p-2 text-left text-sm leading-relaxed text-gray-800 sm:p-4 sm:text-base [&>p:not(:last-child)]:mb-3 [&>p>a]:text-blue-700 [&>p>a]:underline">
+                {guideRenderer.render(faq.description)}
+              </div>
+            </Question>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
